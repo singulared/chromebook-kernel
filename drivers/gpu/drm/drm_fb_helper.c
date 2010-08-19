3557bb6cf6a053a2e3a02c79d36a4e5dd9652eb6
Hugh Dickins <hughd@chromium.org>
2010-08-19 13:07:08 -0700

    CHROMIUM: drm: drm_fb_helper_panic hangs panic
    One of the notifiers called on panic, drm_fb_helper_panic(), hinders
    rather than helps the panic: I've never seen it do other than hang
    the machine, before all the messages were shown, preventing reboot.
    
    Jesse Barnes posted 3 patches to linux-kernel on 9 April 2010, to
    make it "a little more likely we'll get panic output onto the screen
    even when X is running" with Kernel Mode Switching:
    http://lkml.org/lkml/2010/4/9/359
    
    One small uncommented fragment from those improves our chance of
    rebooting from never to often (and I believe my test timing tended
    to lower its chance of success): get out early if !crtc->enabled.
    
    The rest of his patches (console stuff and mutex avoidance) may well
    make sense, but in practice did not appear to help us at all - would
    need more work to be worthwhile in our context.
    
    And if we want to reboot immediately, we shall prefer to avoid this
    mode switching complexity altogether (a subsequent patch): so merge
    the most valuable fragment but forget the rest.
    
    BUG=chromium-os:2269
    TEST=manual
    
    Signed-off-by: Jesse Barnes <jbarnes@virtuousgeek.org>
    Signed-off-by: Hugh Dickins <hughd@chromium.org>
    [olofj: fixups for 3.0]
    Signed-off-by: Olof Johansson <olofj@chromium.org>
    [marcheu: fixups for 3.8]
    Signed-off-by: Stéphane Marchesin <marcheu@chromium.org>
    Review URL: http://codereview.chromium.org/2836086
    
    Change-Id: Ic097364d0c717bd56eb14349b08ebd5bf593de7b

--- drivers/gpu/drm/drm_fb_helper.c
+++ drivers/gpu/drm/drm_fb_helper.c
@@ -245,7 +245,13 @@ bool drm_fb_helper_restore_fbdev_mode(struct drm_fb_helper *fb_helper)
 	int i, ret;
 	for (i = 0; i < fb_helper->crtc_count; i++) {
 		struct drm_mode_set *mode_set = &fb_helper->crtc_info[i].mode_set;
-		ret = mode_set->crtc->funcs->set_config(mode_set);
+		struct drm_crtc *crtc = mode_set->crtc;
+		ret = crtc->funcs->set_config(mode_set);
+
+		if (!crtc->enabled)
+			continue;
+
+		ret = drm_crtc_helper_set_config(mode_set);
 		if (ret)
 			error = true;
 	}
