004d03fb88034aa15c996e92f16c74eae32323ac
Colin Cross <ccross@android.com>
2012-02-16 13:30:11 -0800

    CHROMIUM: ARM: exynos: add sched_clock to mct
    Add a more accurate sched_clock than the default one when MCT
    is used.
    
    Signed-off-by: Colin Cross <ccross@android.com>
    
    BUG=chrome-os-partner:10818
    TEST=collect trace data and verify start+duration times are sub-10ms
    
    Change-Id: I473e2f16a6d6bebc7df86dee32114cda23e6d553
    Reviewed-on: https://gerrit.chromium.org/gerrit/26455
    Tested-by: Sam Leffler <sleffler@chromium.org>
    Reviewed-by: Jeff Disher <disher@chromium.org>
    Tested-by: Jeff Disher <disher@chromium.org>
    Commit-Ready: Sam Leffler <sleffler@chromium.org>
    Reviewed-by: Sam Leffler <sleffler@chromium.org>

--- arch/arm/mach-exynos/mct.c
+++ arch/arm/mach-exynos/mct.c
@@ -21,6 +21,7 @@
 #include <linux/percpu.h>
 #include <linux/of.h>
 
+#include <asm/sched_clock.h>
 #include <asm/arch_timer.h>
 #include <asm/hardware/gic.h>
 #include <asm/localtimer.h>
@@ -128,6 +129,11 @@ static void exynos4_mct_frc_start(u32 hi, u32 lo)
 	exynos4_mct_write(reg, EXYNOS4_MCT_G_TCON);
 }
 
+static u32 exynos4_read_sched_clock(void)
+{
+	return __raw_readl(EXYNOS4_MCT_G_CNT_L);
+}
+
 static cycle_t exynos4_frc_read(struct clocksource *cs)
 {
 	unsigned int lo, hi;
@@ -162,6 +168,8 @@ static void __init exynos4_clocksource_init(void)
 
 	if (clocksource_register_hz(&mct_frc, clk_rate))
 		panic("%s: can't register clocksource\n", mct_frc.name);
+
+	setup_sched_clock(exynos4_read_sched_clock, 32, clk_rate);
 }
 
 static void exynos4_mct_comp0_stop(void)
