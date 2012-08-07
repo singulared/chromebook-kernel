34e6d858ccd377161f160c6d606eeefda26d23d6
Olof Johansson <olofj@chromium.org>
2012-08-07 12:29:46 -0700

    CHROMIUM: cpufreq: ondemand: io_is_busy should be 1 for ARM
    Be conservative for now and only set it for V7 and newer versions.
    
    BUG=chrome-os-partner:11403
    TEST=build, boot, check boot time.
    
    Change-Id: Ia4460efadf106c75608aac85a1f90d773af46cf2
    Signed-off-by: Olof Johansson <olofj@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/29435
    Reviewed-by: Sameer Nanda <snanda@chromium.org>

--- drivers/cpufreq/cpufreq_ondemand.c
+++ drivers/cpufreq/cpufreq_ondemand.c
@@ -65,7 +65,8 @@ static void ondemand_powersave_bias_init_cpu(int cpu)
  * efficient idling at a higher frequency/voltage is.
  * Pavel Machek says this is not so for various generations of AMD and old
  * Intel systems.
- * Mike Chan (androidlcom) calis this is also not true for ARM.
+ * ARM systems v7 and later generally good idle power management as well, so
+ * treat them the same.
  * Because of this, whitelist specific known (series) of CPUs by default, and
  * leave all others up to the user.
  */
@@ -79,6 +80,9 @@ static int should_io_be_busy(void)
 			boot_cpu_data.x86 == 6 &&
 			boot_cpu_data.x86_model >= 15)
 		return 1;
+#endif
+#if defined(CONFIG_ARM) && defined(CPU_V7)
+	return 1;
 #endif
 	return 0;
 }
