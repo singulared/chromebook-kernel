cf0bdc4ec0df91220b67c8fe657727c80b18c7cb
Takashi Iwai <tiwai@suse.de>
2012-12-20 11:02:00 +0100

    BACKPORT: ALSA: hda - Clear cached_write flag in snd_hda_codec_resume_*()
    These functions are supposed to be called at finishing the cached
    sequential writes, so clear the flag properly for lazy developers who
    often forget details.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    (cherry picked from commit aa88a3553eebdcc3ce6801aabb4ed0223bfa198e)
    
    BUG=chrome-os-partner:19100
    TEST=Verified the working of HDMI audio with both stereo and headset by building
         the ChromeOS image along with entire 3.9 HDMI driver patches and four
         additional patches from drm/i915.

--- sound/pci/hda/hda_codec.c
+++ sound/pci/hda/hda_codec.c
@@ -1955,6 +1955,7 @@ void snd_hda_codec_resume_amp(struct hda_codec *codec)
 	int i;
 
 	mutex_lock(&codec->hash_mutex);
+	codec->cached_write = 0;
 	for (i = 0; i < codec->amp_cache.buf.used; i++) {
 		struct hda_amp_info *buffer;
 		u32 key;
@@ -3519,6 +3520,7 @@ void snd_hda_codec_resume_cache(struct hda_codec *codec)
 	int i;
 
 	mutex_lock(&codec->hash_mutex);
+	codec->cached_write = 0;
 	for (i = 0; i < codec->cmd_cache.buf.used; i++) {
 		struct hda_cache_head *buffer;
 		u32 key;
