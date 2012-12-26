8e82d561b9f622b53ecea372cb6bcd5a8c15be21
KyongHo Cho <pullip.cho@samsung.com>
2012-12-26 10:53:22 +0900

    CHROMIUM: iommu/exynos: add missing cache flush for removed pagetable entries
    This commit adds cache flush for removed small page and large page
    entries in exynos_iommu_unmap(). Missing cache flush of removed
    page table entries can cause missing page fault interrupt when a
    master IP accesses an unmapped area.
    
    [ patch posted 12/26/2012 but not accepted/applied upstream:
        https://patchwork.kernel.org/patch/1910401/
    ]
    
    BUG=chromium:219207
    TEST=build/boot.
    
    Change-Id: I5a3a91c0df667a5044d1155933749189ff3439ba
    Signed-off-by: KyongHo Cho <pullip.cho@samsung.com>
    Signed-off-by: Grant Grundler <grundler@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/60565
    Reviewed-by: Doug Anderson <dianders@chromium.org>

--- drivers/iommu/exynos-iommu.c
+++ drivers/iommu/exynos-iommu.c
@@ -962,6 +962,7 @@ static size_t exynos_iommu_unmap(struct iommu_domain *domain,
 
 	if (lv2ent_small(ent)) {
 		*ent = 0;
+		pgtable_flush(ent, ent + 1);
 		size = SPAGE_SIZE;
 		priv->lv2entcnt[lv1ent_offset(iova)] += 1;
 		goto done;
@@ -971,6 +972,7 @@ static size_t exynos_iommu_unmap(struct iommu_domain *domain,
 	BUG_ON(size < LPAGE_SIZE);
 
 	memset(ent, 0, sizeof(*ent) * SPAGES_PER_LPAGE);
+	pgtable_flush(ent, ent + SPAGES_PER_LPAGE);
 
 	size = LPAGE_SIZE;
 	priv->lv2entcnt[lv1ent_offset(iova)] += SPAGES_PER_LPAGE;
