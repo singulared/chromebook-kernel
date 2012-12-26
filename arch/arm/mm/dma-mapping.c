5e3a40e758016937073e0d32addd5e346862e6a4
Prathyush K <prathyush.k@samsung.com>
2012-12-26 13:10:34 +0530

    UPSTREAM: arm: dma mapping: export arm iommu functions
    This patch adds EXPORT_SYMBOL_GPL calls to the three arm iommu
    functions - arm_iommu_create_mapping, arm_iommu_free_mapping
    and arm_iommu_attach_device. These three functions are arm specific
    wrapper functions for creating/freeing/using an iommu mapping and
    they are called by various drivers. If any of these drivers need
    to be built as dynamic modules, these functions need to be exported.
    
    Changelog v2: using EXPORT_SYMBOL_GPL as suggested by Marek.
    
    Signed-off-by: Prathyush K <prathyush.k@samsung.com>

--- arch/arm/mm/dma-mapping.c
+++ arch/arm/mm/dma-mapping.c
@@ -1799,6 +1799,7 @@ err2:
 err:
 	return ERR_PTR(err);
 }
+EXPORT_SYMBOL_GPL(arm_iommu_create_mapping);
 
 static void release_iommu_mapping(struct kref *kref)
 {
@@ -1815,6 +1816,7 @@ void arm_iommu_release_mapping(struct dma_iommu_mapping *mapping)
 	if (mapping)
 		kref_put(&mapping->kref, release_iommu_mapping);
 }
+EXPORT_SYMBOL_GPL(arm_iommu_release_mapping);
 
 /**
  * arm_iommu_attach_device
@@ -1843,5 +1845,6 @@ int arm_iommu_attach_device(struct device *dev,
 	pr_debug("Attached IOMMU controller to %s device.\n", dev_name(dev));
 	return 0;
 }
+EXPORT_SYMBOL_GPL(arm_iommu_attach_device);
 
 #endif
