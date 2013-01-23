34bdceca99c6c0b8e9819633dc9e29997dfe5c15
Sean Paul <seanpaul@chromium.org>
2013-01-23 13:55:05 -0500

    drm/exynos: Reset mixer when we dpms off
    Reset the mixer when we power off, this will perform a soft reset on the
    IP block and disable IRQs.
    
    BUG=chromium-os:37797
    TEST=Tested by hand with DRM_ERROR logging. Made sure IRQ no longer
    	fires after dpms off.
    
    Change-Id: I35a438b07085173776257bba93ffe603d096f2be
    Signed-off-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/41835
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>

--- drivers/gpu/drm/exynos/exynos_mixer.c
+++ drivers/gpu/drm/exynos/exynos_mixer.c
@@ -1056,6 +1056,7 @@ static void mixer_resource_poweroff(struct mixer_context *mctx)
 		clk_disable(res->vp);
 		clk_disable(res->sclk_mixer);
 	}
+	mixer_win_reset(mctx);
 	mctx->is_mixer_powered_on = false;
 }
 
