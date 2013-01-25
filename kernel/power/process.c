ac0558e8465b644be24406aad705867b98228bc6
Sonny Rao <sonnyrao@chromium.org>
2013-01-25 15:31:17 -0800

    Revert "PM / Sleep: Mitigate race between the freezer and request_firmware()"
    This reverts commit 247bc03742545fec2f79939a3b9f738392a0f7b4.
    
    This patch caused a deadlock between khubd and the suspend process.
    This was reliably reproducible when we did a suspend immediately after
    a resume, and the khubd kernel thread was busy probing USB devices.
    
    The sequence was that khubd would be running and probing USB devices
    when we try to suspend and the freezer would activate, and eventually
    khubd would call request_firmware() while holding a device lock, and
    request_firmware would call usermodehelper_read_trylock().
    Because the UMH is in state UMH_FREEZING, usermode_helper_read_trylock
    would end up calling try_to_freeze() and would then be frozen while
    holding a device lock.  After the freezer finished, it would then try
    to suspend each device and would deadlock on the USB hub device that
    was being held by khubd.
    
    Reverting this merely results in a WARN_ON from request_firmware() and
    no deadlock, and upon the subsequent resume, khubd will successfully
    get the firmware and the devices work correctly.
    
    Signed-off-by: Sonny Rao <sonnyrao@chromium.org>
    
    BUG=chrome-os-partner:17270
    TEST=(needs an autotest test) currently manual:
    use the set_short_powerd_timeouts script to all the device to
    idle-suspend quickly, wait for an idle suspend with the lid open
    (about 30  seconds). Then hit the trackpad and quickly close the lid.
    The device should resume and then suspend again within a few seconds.
    
    Change-Id: Idfc5d40f0f1cc88ca335502454bf675f152df477
    Reviewed-on: https://gerrit.chromium.org/gerrit/42190
    Reviewed-by: Sameer Nanda <snanda@chromium.org>
    Commit-Queue: Sonny Rao <sonnyrao@chromium.org>
    Tested-by: Sonny Rao <sonnyrao@chromium.org>

--- kernel/power/process.c
+++ kernel/power/process.c
@@ -123,7 +123,7 @@ int freeze_processes(void)
 {
 	int error;
 
-	error = __usermodehelper_disable(UMH_FREEZING);
+	error = usermodehelper_disable();
 	if (error)
 		return error;
 
@@ -135,7 +135,6 @@ int freeze_processes(void)
 	error = try_to_freeze_tasks(true);
 	if (!error) {
 		printk("done.");
-		__usermodehelper_set_disable_depth(UMH_DISABLED);
 		oom_killer_disable();
 	}
 	printk("\n");
