345fbecd627c807cfb8f7b0ccaa113cdb30226cc
Vivek Gautam <gautam.vivek@samsung.com>
2013-01-31 11:33:54 +0530

    usb: phy: samsung: change the phy lib for multiple PHYs support
    Aligning here with changes for multiple PHYs support
    of the same type.
    
    BUG=chrome-os-partner:19007
    TEST=build and boot on smdk5420 and peach pit;
         tested usb HID devices and mass storage devices on
         USB 2.0 and USB 3.0 ports.
    
    Change-Id: I8cec81afb170b28f663da31bc74feb1740a3a9bd
    Signed-off-by: Vivek Gautam <gautam.vivek@samsung.com>
    Reviewed-on: https://gerrit.chromium.org/gerrit/56395
    Reviewed-by: Doug Anderson <dianders@chromium.org>
    Tested-by: Doug Anderson <dianders@chromium.org>
    Commit-Queue: Julius Werner <jwerner@chromium.org>
    Reviewed-by: Julius Werner <jwerner@chromium.org>

--- drivers/usb/phy/samsung-usb2phy.c
+++ drivers/usb/phy/samsung-usb2phy.c
@@ -428,6 +428,7 @@ static int samsung_usb2phy_probe(struct platform_device *pdev)
 	sphy->clk		= clk;
 	sphy->drv_data		= drv_data;
 	sphy->phy.dev		= sphy->dev;
+	sphy->phy.type		= USB_PHY_TYPE_USB2;
 	sphy->phy.label		= "samsung-usb2phy";
 	sphy->phy.init		= samsung_usb2phy_init;
 	sphy->phy.shutdown	= samsung_usb2phy_shutdown;
@@ -441,7 +442,7 @@ static int samsung_usb2phy_probe(struct platform_device *pdev)
 
 	platform_set_drvdata(pdev, sphy);
 
-	return usb_add_phy(&sphy->phy, USB_PHY_TYPE_USB2);
+	return usb_add_phy_dev(&sphy->phy);
 }
 
 static int samsung_usb2phy_remove(struct platform_device *pdev)
--- drivers/usb/phy/samsung-usb3phy.c
+++ drivers/usb/phy/samsung-usb3phy.c
@@ -277,6 +277,7 @@ static int samsung_usb3phy_probe(struct platform_device *pdev)
 	sphy->regs		= phy_base;
 	sphy->clk		= clk;
 	sphy->phy.dev		= sphy->dev;
+	sphy->phy.type		= USB_PHY_TYPE_USB3;
 	sphy->phy.label		= "samsung-usb3phy";
 	sphy->phy.init		= samsung_usb3phy_init;
 	sphy->phy.shutdown	= samsung_usb3phy_shutdown;
@@ -287,7 +288,7 @@ static int samsung_usb3phy_probe(struct platform_device *pdev)
 
 	platform_set_drvdata(pdev, sphy);
 
-	return usb_add_phy(&sphy->phy, USB_PHY_TYPE_USB3);
+	return usb_add_phy_dev(&sphy->phy);
 }
 
 static int samsung_usb3phy_remove(struct platform_device *pdev)
