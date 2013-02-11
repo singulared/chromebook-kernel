61d0d45b32362fbbac82aa7f58f5061003a1f86e
Will Deacon <will.deacon@arm.com>
2013-02-11 13:47:48 +0000

    UPSTREAM: ARM: tlb: don't perform inner-shareable invalidation for local TLB ops
    Inner-shareable TLB invalidation is typically more expensive than local
    (non-shareable) invalidation, so performing the broadcasting for
    local_flush_tlb_* operations is a waste of cycles and needlessly
    clobbers entries in the TLBs of other CPUs.
    
    This patch introduces __flush_tlb_* versions for many of the TLB
    invalidation functions, which only respect inner-shareable variants of
    the invalidation instructions when presented with the TLB_V7_UIS_FULL
    flag. The local version is also inlined to prevent SMP_ON_UP kernels
    from missing flushes, where the __flush variant would be called with
    the UP flags.
    
    This gains us around 0.5% in hackbench scores for a dual-core A15, but I
    would expect this to improve as more cores (and clusters) are added to
    the equation.
    
    Reviewed-by: Catalin Marinas <catalin.marinas@arm.com>
    Reported-by: Albin Tonnerre <Albin.Tonnerre@arm.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    
    (cherry-picked from commit f0915781bd5edf78b1154e61efe962dc15872d09)
    BUG=chrome-os-partner:24078
    TEST=Run WebGL + Video aging test for 48+hrs.
    See no issues.
    Signed-off-by: Arjun.K.V <arjun.kv@samsung.com>
    Change-Id: Iccaebe691b68c0e69d27b5189d8542d613c06ddc
    Reviewed-on: https://chromium-review.googlesource.com/177006
    Reviewed-by: Andrew Bresticker <abrestic@chromium.org>
    Tested-by: Andrew Bresticker <abrestic@chromium.org>

--- arch/arm/kernel/smp_tlb.c
+++ arch/arm/kernel/smp_tlb.c
@@ -99,7 +99,7 @@ void flush_tlb_all(void)
 	if (tlb_ops_need_broadcast())
 		on_each_cpu(ipi_flush_tlb_all, NULL, 1);
 	else
-		local_flush_tlb_all();
+		__flush_tlb_all();
 	broadcast_tlb_a15_erratum();
 }
 
@@ -108,7 +108,7 @@ void flush_tlb_mm(struct mm_struct *mm)
 	if (tlb_ops_need_broadcast())
 		on_each_cpu_mask(mm_cpumask(mm), ipi_flush_tlb_mm, mm, 1);
 	else
-		local_flush_tlb_mm(mm);
+		__flush_tlb_mm(mm);
 	broadcast_tlb_mm_a15_erratum(mm);
 }
 
@@ -121,7 +121,7 @@ void flush_tlb_page(struct vm_area_struct *vma, unsigned long uaddr)
 		on_each_cpu_mask(mm_cpumask(vma->vm_mm), ipi_flush_tlb_page,
 					&ta, 1);
 	} else
-		local_flush_tlb_page(vma, uaddr);
+		__flush_tlb_page(vma, uaddr);
 	broadcast_tlb_mm_a15_erratum(vma->vm_mm);
 }
 
@@ -132,7 +132,7 @@ void flush_tlb_kernel_page(unsigned long kaddr)
 		ta.ta_start = kaddr;
 		on_each_cpu(ipi_flush_tlb_kernel_page, &ta, 1);
 	} else
-		local_flush_tlb_kernel_page(kaddr);
+		__flush_tlb_kernel_page(kaddr);
 	broadcast_tlb_a15_erratum();
 }
 
