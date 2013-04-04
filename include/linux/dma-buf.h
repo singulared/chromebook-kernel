858e10717c43568d5f591cfbf65328921038dbb0
Sumit Semwal <sumit.semwal@linaro.org>
2013-04-04 11:44:37 +0530

    dma-buf: Add debugfs support
    Add debugfs support to make it easier to print debug information
    about the dma-buf buffers.
    
    Cc: Dave Airlie <airlied@redhat.com>
     [minor fixes on init and warning fix]
    Cc: Dan Carpenter <dan.carpenter@oracle.com>
     [remove double unlock in fail case]
    Signed-off-by: Sumit Semwal <sumit.semwal@linaro.org>
    
    (cherry picked from commit b89e35636bc75b72d15a1af6d49798802aff77d5)
    Signed-off-by: Stéphane Marchesin <marcheu@chromium.org>
    
    Conflicts:
    	drivers/base/dma-buf.c
    
    BUG=chromium:340541
    TEST=builds and runs on link and daisy
    
    Change-Id: I3b032d8b8cd7f43b3ca2e2742943c41ac641d8df
    Reviewed-on: https://chromium-review.googlesource.com/190621
    Tested-by: Stéphane Marchesin <marcheu@chromium.org>
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>
    Commit-Queue: Stéphane Marchesin <marcheu@chromium.org>

--- include/linux/dma-buf.h
+++ include/linux/dma-buf.h
@@ -116,6 +116,7 @@ struct dma_buf_ops {
  * @attachments: list of dma_buf_attachment that denotes all devices attached.
  * @ops: dma_buf_ops associated with this buffer object.
  * @exp_name: name of the exporter; useful for debugging.
+ * @list_node: node for dma_buf accounting and debugging.
  * @priv: exporter specific private data for this buffer object.
  */
 struct dma_buf {
@@ -131,6 +132,7 @@ struct dma_buf {
 	unsigned vmapping_counter;
 	void *vmap_ptr;
 	const char *exp_name;
+	struct list_head list_node;
 	void *priv;
 };
 
@@ -215,5 +217,6 @@ int dma_buf_mmap(struct dma_buf *, struct vm_area_struct *,
 		 unsigned long);
 void *dma_buf_vmap(struct dma_buf *);
 void dma_buf_vunmap(struct dma_buf *, void *vaddr);
-
+int dma_buf_debugfs_create_file(const char *name,
+				int (*write)(struct seq_file *));
 #endif /* __DMA_BUF_H__ */
