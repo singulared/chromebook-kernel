361af9359d071ebbe9c964b0231807301d507eed
Imre Deak <imre.deak@intel.com>
2013-04-19 11:11:56 +1000

    drm: prime: fix refcounting on the dmabuf import error path
    In commit be8a42ae60 we inroduced a refcount problem, where on the
    drm_gem_prime_fd_to_handle() error path we'll call dma_buf_put() for
    self imported dma buffers.
    
    Fix this by taking a reference on the dma buffer in the .gem_import
    hook instead of assuming the caller had taken one. Besides fixing the
    bug this is also more logical.
    
    Signed-off-by: Imre Deak <imre.deak@intel.com>
    Cc: stable@vger.kernel.org
    Reviewed-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Signed-off-by: Dave Airlie <airlied@redhat.com>
    (cherry picked from commit 011c2282c74db120f01a8414edc66c3f217f5511)
    Signed-off-by: Stéphane Marchesin <marcheu@chromium.org>
    
    Conflicts:
    	drivers/gpu/drm/i915/i915_gem_dmabuf.c
    	drivers/gpu/drm/omapdrm/omap_gem_dmabuf.c
    [marcheu: fixup extra file from bad cherry-picking]
    
    BUG=chromium:340541
    TEST=builds and runs on link and daisy
    
    Change-Id: I43bb09bec48aa9d1cb93295c9cf89f13d76189ac
    Reviewed-on: https://chromium-review.googlesource.com/190606
    Tested-by: Stéphane Marchesin <marcheu@chromium.org>
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>
    Commit-Queue: Stéphane Marchesin <marcheu@chromium.org>

--- drivers/gpu/drm/exynos/exynos_drm_dmabuf.c
+++ drivers/gpu/drm/exynos/exynos_drm_dmabuf.c
@@ -270,7 +270,6 @@ struct drm_gem_object *exynos_dmabuf_prime_import(struct drm_device *drm_dev,
 			 * refcount on gem itself instead of f_count of dmabuf.
 			 */
 			drm_gem_object_reference(obj);
-			dma_buf_put(dma_buf);
 			return obj;
 		}
 	}
@@ -279,6 +278,7 @@ struct drm_gem_object *exynos_dmabuf_prime_import(struct drm_device *drm_dev,
 	if (IS_ERR(attach))
 		return ERR_PTR(-EINVAL);
 
+	get_dma_buf(dma_buf);
 
 	sgt = dma_buf_map_attachment(attach, DMA_BIDIRECTIONAL);
 	if (IS_ERR_OR_NULL(sgt)) {
@@ -333,6 +333,8 @@ err_unmap_attach:
 	dma_buf_unmap_attachment(attach, sgt, DMA_BIDIRECTIONAL);
 err_buf_detach:
 	dma_buf_detach(dma_buf, attach);
+	dma_buf_put(dma_buf);
+
 	return ERR_PTR(ret);
 }
 
