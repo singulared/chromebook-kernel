AndreiLux/Perseus-UNIVERSAL5410

CHROMIUM: usb: option: handle identity morphing on Novatel E362 modem
This patch is suggested by Novatel Wireless to address identity morphing
on Novatel E362 modem such that the correct USB configuration is
selected and detected by the option driver.

BUG=chromium-os:15365
TEST=Verified that the option driver correctly detects Novatel E362
modem with firmware 1.41 and 4.08.

Change-Id: I3449de2327e2b42417e26ff6024f84b72b140853
Signed-off-by: Ben Chan <benchan@chromium.org>
Reviewed-on: https://gerrit.chromium.org/gerrit/36042
Reviewed-by: Paul Stewart <pstew@chromium.org>

drivers/usb/serial/option.c
@@ -735,7 +735,7 @@ static const struct usb_device_id option_ids[] = {
 	/*
 	{ USB_DEVICE(NOVATELWIRELESS_VENDOR_ID, NOVATELWIRELESS_PRODUCT_G2) },
 	*/
-	{ USB_DEVICE_AND_INTERFACE_INFO(NOVATELWIRELESS_VENDOR_ID, NOVATELWIRELESS_PRODUCT_LTE_E362, 0xff, 0xff, 0xff) },
+	{ USB_DEVICE(NOVATELWIRELESS_VENDOR_ID, NOVATELWIRELESS_PRODUCT_LTE_E362) },
 	{ USB_DEVICE_AND_INTERFACE_INFO(NOVATELWIRELESS_VENDOR_ID, NOVATELWIRELESS_PRODUCT_GW, 0xff, 0xff, 0xff) },
 
 	{ USB_DEVICE(AMOI_VENDOR_ID, AMOI_PRODUCT_H01) },
@@ -1373,6 +1373,16 @@ static int option_probe(struct usb_serial *serial,
 		serial->dev->descriptor.idProduct == SAMSUNG_PRODUCT_GT_B3730 &&
 		serial->interface->cur_altsetting->desc.bInterfaceClass != USB_CLASS_CDC_DATA)
 		return -ENODEV;
+
+	/* For Novatel E362 modem, set to configuration #1 if identity morphing
+	 * is detected.
+	 */
+	if (serial->dev->descriptor.idVendor == NOVATELWIRELESS_VENDOR_ID &&
+		serial->dev->descriptor.idProduct == NOVATELWIRELESS_PRODUCT_LTE_E362 &&
+		serial->dev->actconfig->desc.bConfigurationValue != 1) {
+		if (usb_driver_set_configuration(serial->dev, 1) != 0)
+			return -ENODEV;
+	}
 
 	data = serial->private = kzalloc(sizeof(struct usb_wwan_intf_private), GFP_KERNEL);
 	if (!data)

singulared/chromebook-kernel

drivers/usb/serial/option.c
@@ -745,7 +745,7 @@ static const struct usb_device_id option_ids[] = {
 	*/
 	/* Novatel Ovation MC551 a.k.a. Verizon USB551L */
 	{ USB_DEVICE_AND_INTERFACE_INFO(NOVATELWIRELESS_VENDOR_ID, NOVATELWIRELESS_PRODUCT_MC551, 0xff, 0xff, 0xff) },
-	{ USB_DEVICE_AND_INTERFACE_INFO(NOVATELWIRELESS_VENDOR_ID, NOVATELWIRELESS_PRODUCT_E362, 0xff, 0xff, 0xff) },
+	{ USB_DEVICE(NOVATELWIRELESS_VENDOR_ID, NOVATELWIRELESS_PRODUCT_E362) },
 
 	{ USB_DEVICE(AMOI_VENDOR_ID, AMOI_PRODUCT_H01) },
 	{ USB_DEVICE(AMOI_VENDOR_ID, AMOI_PRODUCT_H01A) },
@@ -1455,6 +1455,17 @@ static int option_attach(struct usb_serial *serial)
 	const struct usb_device_id *id;
 	struct usb_wwan_intf_private *data;
 	struct option_private *priv;
+
+
+	/* For Novatel E362 modem, set to configuration #1 if identity morphing
+	 * is detected.
+	 */
+	if (serial->dev->descriptor.idVendor == NOVATELWIRELESS_VENDOR_ID &&
+		serial->dev->descriptor.idProduct == NOVATELWIRELESS_PRODUCT_E362 &&
+		serial->dev->actconfig->desc.bConfigurationValue != 1) {
+		if (usb_driver_set_configuration(serial->dev, 1) != 0)
+			return -ENODEV;
+	}
 
 	data = kzalloc(sizeof(struct usb_wwan_intf_private), GFP_KERNEL);
 	if (!data)