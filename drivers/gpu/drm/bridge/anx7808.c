014b3bd6dd20cf60ebffbfef8fc7a91fba1703c7
Jeremy Thorpe <jeremyt@chromium.org>
2013-05-02 10:29:11 -0700

    CHROMIUM: drm/anx7808: Add hardware probe.
    Adds register definitions and functions to read registers.
    
    Adds a hardware probe function that reads configuration registers to determine
    whether hardware is present and responds as expected.
    
    BUG=chrome-os-partner:16196
    TEST=Run on spring with verbose logging, and observe "ANX7808 found." in logs.
    
    Change-Id: I577bbb022e78f4a1c41be1096e740bd3a4463697
    Signed-off-by: Jeremy Thorpe <jeremyt@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/48756

--- drivers/gpu/drm/bridge/anx7808.c
+++ drivers/gpu/drm/bridge/anx7808.c
@@ -23,6 +23,9 @@
 #include <linux/of_gpio.h>
 #include <linux/regulator/consumer.h>
 #include "drmP.h"
+#include "anx7808regs.h"
+
+#define ANX7808_DEVICE_ID 0x7808
 
 struct anx7808_data {
 	int pd_gpio;
@@ -30,7 +33,111 @@ struct anx7808_data {
 	int intp_gpio;
 	int cable_det_gpio;
 	struct regulator *vdd_mydp;
+	struct i2c_client *tx_p0;
+	struct i2c_client *tx_p1;
+	struct i2c_client *tx_p2;
+	struct i2c_client *rx_p0;
+	struct i2c_client *rx_p1;
 };
+
+static struct i2c_client *anx7808_addr_to_client(struct anx7808_data *anx7808,
+						 uint16_t addr)
+{
+	switch (addr >> 8) {
+	case TX_P0:
+		return anx7808->tx_p0;
+	case TX_P1:
+		return anx7808->tx_p1;
+	case TX_P2:
+		return anx7808->tx_p2;
+	case RX_P0:
+		return anx7808->rx_p0;
+	case RX_P1:
+		return anx7808->rx_p1;
+	default:
+		break;
+	}
+	DRM_ERROR("Invalid i2c address %04x.\n", addr);
+	return NULL;
+}
+
+static int anx7808_read_reg(struct anx7808_data *anx7808, uint16_t addr,
+			    uint8_t *value)
+{
+	int ret = 0;
+	struct i2c_client *client = anx7808_addr_to_client(anx7808, addr);
+
+	if (client == NULL) {
+		*value = 0;
+		return -EINVAL;
+	}
+
+	ret = i2c_smbus_read_byte_data(client, addr & 0xff);
+	if (ret < 0) {
+		DRM_ERROR("Failed to read i2c addr=%04x.\n", addr);
+		*value = 0;
+		return ret;
+	}
+	*value = ret;
+	return 0;
+}
+
+static int anx7808_power_on(struct anx7808_data *anx7808)
+{
+	int ret = 0;
+
+	DRM_INFO("Powering on ANX7808.\n");
+
+	gpio_set_value(anx7808->reset_gpio, 0);
+	usleep_range(1000, 2000);
+	gpio_set_value(anx7808->pd_gpio, 0);
+	usleep_range(2000, 4000);
+	ret = regulator_enable(anx7808->vdd_mydp);
+	if (ret < 0) {
+		DRM_ERROR("Failed to power on ANX7808: %d", ret);
+		return ret;
+	}
+	msleep(20);
+	gpio_set_value(anx7808->reset_gpio, 1);
+	return 0;
+}
+
+static int anx7808_power_off(struct anx7808_data *anx7808)
+{
+	int ret = 0;
+
+	DRM_INFO("Powering off ANX7808.\n");
+
+	gpio_set_value(anx7808->reset_gpio, 0);
+	usleep_range(1000, 2000);
+	ret = regulator_disable(anx7808->vdd_mydp);
+	if (ret < 0) {
+		DRM_ERROR("Failed to power off ANX7808: %d", ret);
+		return ret;
+	}
+	usleep_range(5000, 10000);
+	gpio_set_value(anx7808->pd_gpio, 1);
+	usleep_range(1000, 2000);
+	return ret;
+}
+
+static int anx7808_chip_located(struct anx7808_data *anx7808)
+{
+	int ret = 0;
+	uint16_t id;
+	uint8_t idh = 0, idl = 0;
+	ret |= anx7808_read_reg(anx7808, SP_TX_DEV_IDL_REG, &idl);
+	ret |= anx7808_read_reg(anx7808, SP_TX_DEV_IDH_REG, &idh);
+	if (ret)
+		return ret;
+	id = idl | (idh << 8);
+	if (id != ANX7808_DEVICE_ID) {
+		DRM_ERROR("ANX7808 not found.  ID reg contains: %04x\n", id);
+		return -ENODEV;
+	}
+	DRM_INFO("ANX7808 found.\n");
+	return 0;
+}
 
 static void anx7808_free_gpios(struct anx7808_data *anx7808)
 {
@@ -38,6 +145,18 @@ static void anx7808_free_gpios(struct anx7808_data *anx7808)
 	gpio_free(anx7808->intp_gpio);
 	gpio_free(anx7808->reset_gpio);
 	gpio_free(anx7808->pd_gpio);
+}
+
+static void unregister_i2c_clients(struct anx7808_data *anx7808)
+{
+	if (anx7808->rx_p1)
+		i2c_unregister_device(anx7808->rx_p1);
+	if (anx7808->rx_p0)
+		i2c_unregister_device(anx7808->rx_p0);
+	if (anx7808->tx_p2)
+		i2c_unregister_device(anx7808->tx_p2);
+	if (anx7808->tx_p1)
+		i2c_unregister_device(anx7808->tx_p1);
 }
 
 static int anx7808_probe(struct i2c_client *client,
@@ -117,9 +236,48 @@ static int anx7808_probe(struct i2c_client *client,
 		goto err_gpio;
 	}
 
+	anx7808->tx_p0 = client;
+	if (!((anx7808->tx_p0->addr) == TX_P0 >> 1)) {
+		DRM_ERROR("I2C client address %02x != expected value %02x.\n",
+			  anx7808->tx_p0->addr, TX_P0 >> 1);
+		goto err_i2c;
+	}
+	anx7808->tx_p1 = i2c_new_dummy(client->adapter, TX_P1 >> 1);
+	if (!anx7808->tx_p1) {
+		DRM_ERROR("Failed to reserve i2c bus %02x.\n", TX_P1 >> 1);
+		ret = -EINVAL;
+		goto err_i2c;
+	}
+	anx7808->tx_p2 = i2c_new_dummy(client->adapter, TX_P2 >> 1);
+	if (!anx7808->tx_p2) {
+		DRM_ERROR("Failed to reserve i2c bus %02x.\n", TX_P2 >> 1);
+		ret = -EINVAL;
+		goto err_i2c;
+	}
+	anx7808->rx_p0 = i2c_new_dummy(client->adapter, RX_P0 >> 1);
+	if (!anx7808->rx_p0) {
+		DRM_ERROR("Failed to reserve i2c bus %02x.\n", RX_P0 >> 1);
+		ret = -EINVAL;
+		goto err_i2c;
+	}
+	anx7808->rx_p1 = i2c_new_dummy(client->adapter, RX_P1 >> 1);
+	if (!anx7808->rx_p1) {
+		DRM_ERROR("Failed to reserve i2c bus %02x.\n", RX_P1 >> 1);
+		ret = -EINVAL;
+		goto err_i2c;
+	}
+
+	anx7808_power_on(anx7808);
+	ret = anx7808_chip_located(anx7808);
+	anx7808_power_off(anx7808);
+	if (ret)
+		goto err_i2c;
+
 	DRM_INFO("ANX7808 initialization successful.\n");
 
 	return 0;
+err_i2c:
+	unregister_i2c_clients(anx7808);
 err_gpio:
 	anx7808_free_gpios(anx7808);
 err_reg:
@@ -130,6 +288,7 @@ err_reg:
 static int anx7808_remove(struct i2c_client *client)
 {
 	struct anx7808_data *anx7808 = i2c_get_clientdata(client);
+	unregister_i2c_clients(anx7808);
 	anx7808_free_gpios(anx7808);
 	regulator_put(anx7808->vdd_mydp);
 	return 0;
