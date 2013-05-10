8be42aa282bea51000ddbe50dfc496637d934948
Rahul Sharma <rahul.sharma@samsung.com>
2013-05-10 10:33:37 +0530

    clk/exynos5250: add sclk_hdmiphy in the list of special clocks
    hdmi driver needs hdmiphy clock which one of the parent
    for hdmi mux clock.
    
    BUG=chrome-os-partner:19785
    TEST=basic hdmi works fine on Snow, smdk5420, peach board.
    
    Change-Id: Ibb2f5b41e4f6a477e895ed8c1ba6e57841d37a88
    Signed-off-by: Rahul Sharma <rahul.sharma@samsung.com>
    Reviewed-on: https://gerrit.chromium.org/gerrit/56975
    Commit-Queue: Doug Anderson <dianders@chromium.org>
    Reviewed-by: Doug Anderson <dianders@chromium.org>
    Tested-by: Doug Anderson <dianders@chromium.org>

--- drivers/clk/samsung/clk-exynos5250.c
+++ drivers/clk/samsung/clk-exynos5250.c
@@ -89,6 +89,7 @@ enum exynos5250_clks {
 	sclk_mmc0, sclk_mmc1, sclk_mmc2, sclk_mmc3, sclk_sata, sclk_usb3,
 	sclk_jpeg, sclk_uart0, sclk_uart1, sclk_uart2, sclk_uart3, sclk_pwm,
 	sclk_audio1, sclk_audio2, sclk_spdif, sclk_spi0, sclk_spi1, sclk_spi2,
+	sclk_hdmiphy,
 
 	/* gate clocks */
 	gscl0 = 256, gscl1, gscl2, gscl3, gscl_wa, gscl_wb, smmu_gscl0,
@@ -204,7 +205,7 @@ struct samsung_fixed_rate_clock exynos5250_fixed_rate_ext_clks[] __initdata = {
 
 /* fixed rate clocks generated inside the soc */
 struct samsung_fixed_rate_clock exynos5250_fixed_rate_clks[] __initdata = {
-	FRATE(none, "sclk_hdmiphy", NULL, CLK_IS_ROOT, 24000000),
+	FRATE(sclk_hdmiphy, "sclk_hdmiphy", NULL, CLK_IS_ROOT, 24000000),
 	FRATE(none, "sclk_hdmi27m", NULL, CLK_IS_ROOT, 27000000),
 	FRATE(none, "sclk_dptxphy", NULL, CLK_IS_ROOT, 24000000),
 	FRATE(none, "sclk_uhostphy", NULL, CLK_IS_ROOT, 48000000),
