cc047282815c77164f2780c5e9d20ff828446885
Jouni Malinen <jouni@qca.qualcomm.com>
2013-05-16 20:11:08 +0300

    UPSTREAM: cfg80211: Allow TDLS peer AID to be configured for VHT
    VHT uses peer AID in the PARTIAL_AID field in TDLS frames. The current
    design for TDLS is to first add a dummy STA entry before completing TDLS
    Setup and then update information on this STA entry based on what was
    received from the peer during the setup exchange.
    
    In theory, this could use NL80211_ATTR_STA_AID to set the peer AID just
    like this is used in AP mode to set the AID of an association station.
    However, existing cfg80211 validation rules prevent this attribute from
    being used with set_station operation. To avoid interoperability issues
    between different kernel and user space version combinations, introduce
    a new nl80211 attribute for the purpose of setting TDLS peer AID. This
    attribute can be used in both the new_station and set_station
    operations. It is not supposed to be allowed to change the AID value
    during the lifetime of the STA entry, but that validation is left for
    drivers to do in the change_station callback.
    
    Taken from commit 5e4b6f5698421d94226cc2f80eae6d613c9acef8 in kernel
    3.11.
    
    BUG=chrome-os-partner:37850
    TEST=run with new iwl7000 using this functionality
    CQ-DEPEND=CL:266111
    
    Change-Id: Ic8e7e07c2548d0f83a896106572dc8c65454c17d
    Signed-off-by: Jouni Malinen <jouni@qca.qualcomm.com>
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Signed-off-by: Arik Nemtsov <arik@wizery.com>
    Reviewed-on: https://chromium-review.googlesource.com/266112
    Reviewed-by: Paul Stewart <pstew@chromium.org>
    Tested-by: Roshan Pius <rpius@chromium.org>
    Commit-Queue: Roshan Pius <rpius@chromium.org>

--- net/wireless-3.4/nl80211.c
+++ net/wireless-3.4/nl80211.c
@@ -207,6 +207,7 @@ static const struct nla_policy nl80211_policy[NL80211_ATTR_MAX+1] = {
 	[NL80211_ATTR_NOACK_MAP] = { .type = NLA_U16 },
 	[NL80211_ATTR_INACTIVITY_TIMEOUT] = { .type = NLA_U16 },
 	[NL80211_ATTR_BG_SCAN_PERIOD] = { .type = NLA_U16 },
+	[NL80211_ATTR_PEER_AID] = { .type = NLA_U16 },
 };
 
 /* policy for the key attributes */
@@ -2637,6 +2638,8 @@ static int nl80211_set_station_tdls(struct genl_info *info,
 	int err;
 
 	/* Dummy STA entry gets updated once the peer capabilities are known */
+	if (info->attrs[NL80211_ATTR_PEER_AID])
+		params->aid = nla_get_u16(info->attrs[NL80211_ATTR_PEER_AID]);
 	if (info->attrs[NL80211_ATTR_HT_CAPABILITY])
 		params->ht_capa =
 			nla_data(info->attrs[NL80211_ATTR_HT_CAPABILITY]);
@@ -2837,7 +2840,8 @@ static int nl80211_new_station(struct sk_buff *skb, struct genl_info *info)
 	if (!info->attrs[NL80211_ATTR_STA_SUPPORTED_RATES])
 		return -EINVAL;
 
-	if (!info->attrs[NL80211_ATTR_STA_AID])
+	if (!info->attrs[NL80211_ATTR_STA_AID] &&
+	    !info->attrs[NL80211_ATTR_PEER_AID])
 		return -EINVAL;
 
 	mac_addr = nla_data(info->attrs[NL80211_ATTR_MAC]);
@@ -2848,7 +2852,10 @@ static int nl80211_new_station(struct sk_buff *skb, struct genl_info *info)
 	params.listen_interval =
 		nla_get_u16(info->attrs[NL80211_ATTR_STA_LISTEN_INTERVAL]);
 
-	params.aid = nla_get_u16(info->attrs[NL80211_ATTR_STA_AID]);
+	if (info->attrs[NL80211_ATTR_STA_AID])
+		params.aid = nla_get_u16(info->attrs[NL80211_ATTR_STA_AID]);
+	else
+		params.aid = nla_get_u16(info->attrs[NL80211_ATTR_PEER_AID]);
 	if (!params.aid || params.aid > IEEE80211_MAX_AID)
 		return -EINVAL;
 
