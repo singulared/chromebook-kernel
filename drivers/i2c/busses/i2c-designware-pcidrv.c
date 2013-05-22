4e2ac49fa0d400694adb056f766ae428e00bf530
Duncan Laurie <dlaurie@chromium.org>
2013-05-22 14:48:04 -0700

    CHROMIUM: i2c-designware-pci: Fix warning introduced in previous commit
    When I added the conditional to use id->driver_data it did not
    account for the fact that it is long.
    
    BUG=chrome-os-partner:19495
    TEST=cros_workon_make --board=slippy chromeos-kernel-next --install
    
    Change-Id: Ie4d552aaa372a0e00d1a63d06912cb028f0142df
    Signed-off-by: Duncan Laurie <dlaurie@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/56302
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

--- drivers/i2c/busses/i2c-designware-pcidrv.c
+++ drivers/i2c/busses/i2c-designware-pcidrv.c
@@ -310,7 +310,7 @@ const struct pci_device_id *id)
 	adap->algo = &i2c_dw_algo;
 	adap->dev.parent = &pdev->dev;
 	adap->nr = controller->bus_num;
-	snprintf(adap->name, sizeof(adap->name), "i2c-designware-pci-%d",
+	snprintf(adap->name, sizeof(adap->name), "i2c-designware-pci-%ld",
 		 (adap->nr < 0) ? id->driver_data : adap->nr);
 
 	r = request_irq(pdev->irq, i2c_dw_isr, IRQF_SHARED, adap->name, dev);
