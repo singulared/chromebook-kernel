08b18875333cba0be4aefa0cfa646c08f0f1739f
Rahul Sharma <rahul.sharma@samsung.com>
2013-05-23 10:44:46 +0530

    clk/exynos5250: add clock for tv sysmmu
    Adding sysmmu clock for tv for exynos5250 SoC.
    
    BUG=chrome-os-partner:19785
    TEST=basic hdmi works fine on Snow, smdk5420, peach board.
    
    Change-Id: I6b52c677b3bcfc62a39ae254f6214dca0fb9a61d
    Signed-off-by: Rahul Sharma <rahul.sharma@samsung.com>
    Reviewed-on: https://gerrit.chromium.org/gerrit/56980
    Commit-Queue: Shirish S <shirish@chromium.org>
    Reviewed-by: Shirish S <shirish@chromium.org>
    Tested-by: Shirish S <shirish@chromium.org>

--- drivers/clk/samsung/clk-exynos5250.c
+++ drivers/clk/samsung/clk-exynos5250.c
@@ -25,6 +25,7 @@
 #define SRC_TOP0		0x10210
 #define SRC_TOP1		0x10214
 #define SRC_TOP2		0x10218
+#define SRC_TOP3		0x1021C
 #define SRC_GSCL		0x10220
 #define SRC_DISP1_0		0x1022c
 #define SRC_MAU			0x10240
@@ -102,6 +103,7 @@ enum exynos5250_clks {
 	hsi2c3, chipid, sysreg, pmu, cmu_top, cmu_core, cmu_mem, tzpc0, tzpc1,
 	tzpc2, tzpc3, tzpc4, tzpc5, tzpc6, tzpc7, tzpc8, tzpc9, hdmi_cec, mct,
 	wdt, rtc, tmu, fimd1, mie1, dsim0, dp, mixer, hdmi, g3d, aclk_400_g3d,
+	smmu_tv,
 
 	/* mux clocks */
 	mout_hdmi = 1024, mout_fimd1, mout_audio0,
@@ -177,6 +179,7 @@ PNAME(mout_bpll_user_p)	= { "fin_pll", "sclk_bpll" };
 PNAME(mout_aclk166_p)	= { "sclk_cpll", "sclk_mpll_user" };
 PNAME(mout_aclk200_p)	= { "sclk_mpll_user", "sclk_bpll_user" };
 PNAME(mout_aclk400_p)	= { "aclk_400_g3d_mid", "sclk_gpll" };
+PNAME(mout_aclk200_disp1_sub_p) = { "fin_pll", "aclk200" };
 PNAME(mout_hdmi_p)	= { "div_hdmi_pixel", "sclk_hdmiphy" };
 PNAME(mout_usb3_p)	= { "sclk_mpll_user", "sclk_cpll" };
 PNAME(mout_group1_p)	= { "fin_pll", "fin_pll", "sclk_hdmi27m",
@@ -240,6 +243,7 @@ struct samsung_mux_clock exynos5250_mux_clks[] __initdata = {
 	MUX(none, "aclk_400_g3d_mid", mout_aclk200_p, SRC_TOP0, 20, 1),
 	MUX(none, "sclk_gpll", mout_gpll_p, SRC_TOP2, 28, 1),
 	MUX(none, "mout_aclk400", mout_aclk400_p, SRC_TOP1, 28, 1),
+	MUX(none, "aclk200_disp1", mout_aclk200_disp1_sub_p, SRC_TOP3, 4, 1),
 	MUX(none, "mout_cam_bayer", mout_group1_p, SRC_GSCL, 12, 4),
 	MUX(none, "mout_cam0", mout_group1_p, SRC_GSCL, 16, 4),
 	MUX(none, "mout_cam1", mout_group1_p, SRC_GSCL, 20, 4),
@@ -345,6 +349,7 @@ struct samsung_gate_clock exynos5250_gate_clks[] __initdata = {
 	GATE(smmu_gscl1, "smmu_gscl1", "aclk266", GATE_IP_GSCL, 8, 0, 0),
 	GATE(smmu_gscl2, "smmu_gscl2", "aclk266", GATE_IP_GSCL, 9, 0, 0),
 	GATE(smmu_gscl3, "smmu_gscl3", "aclk266", GATE_IP_GSCL, 10, 0, 0),
+	GATE(smmu_tv, "smmu_tv", "aclk200_disp1", GATE_IP_DISP1, 9, 0, 0),
 	GATE(mfc, "mfc", "aclk333", GATE_IP_MFC, 0, 0, 0),
 	GATE(smmu_mfcl, "smmu_mfcl", "aclk333", GATE_IP_MFC, 1, 0, 0),
 	GATE(smmu_mfcr, "smmu_mfcr", "aclk333", GATE_IP_MFC, 2, 0, 0),
