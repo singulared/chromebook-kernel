46a438bdc4efa18934c855945c532a7d57f501a8
Doug Anderson <dianders@chromium.org>
2013-05-31 11:42:02 -0700

    clk: samsung: Fix pll36xx_recalc_rate to handle kdiv properly
    The KDIV value is often listed as unsigned but it needs to be treated
    as a 16-bit signed value when using it in calculations.  Fix our rate
    recalculation to do this correctly.
    
    Before doing this, I tried setting EPLL on exynos5250 to:
      rate, m, p, s, k = 80000000, 107, 2, 4, 43691
    
    This rate is exactly from the table in the exynos5250 user manual.
    
    I read this back as 80750003 with:
      cat /sys/kernel/debug/clk/fin_pll/fout_epll/clk_rate
    
    After this patch, it reads back as 80000003
    
    This patch was posted upstream at:
      https://patchwork.kernel.org/patch/2645361/
    
    BUG=chrome-os-partner:19772
    TEST=See above.  I tested on snow and set things manually in U-Boot:
      mw 0x10020130 a06b0204
      mw 0x10020134 0000aaab
    
    Change-Id: I691c24157416922ef726335b1a446bd8ed0a9420
    Signed-off-by: Doug Anderson <dianders@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/57233
    Reviewed-by: Andrew Bresticker <abrestic@chromium.org>
    Tested-by: Andrew Bresticker <abrestic@chromium.org>
    Reviewed-by: Vikas Sajjan <vikas.sajjan@samsung.com>

--- drivers/clk/samsung/clk-pll.c
+++ drivers/clk/samsung/clk-pll.c
@@ -303,7 +303,8 @@ static unsigned long samsung_pll36xx_recalc_rate(struct clk_hw *hw,
 				unsigned long parent_rate)
 {
 	struct samsung_clk_pll *pll = to_clk_pll(hw);
-	u32 mdiv, pdiv, sdiv, kdiv, pll_con0, pll_con1;
+	u32 mdiv, pdiv, sdiv, pll_con0, pll_con1;
+	s16 kdiv;
 	u64 fvco = parent_rate;
 
 	pll_con0 = pll_readl(pll, PLL36XX_CON0_OFFSET);
@@ -311,7 +312,7 @@ static unsigned long samsung_pll36xx_recalc_rate(struct clk_hw *hw,
 	mdiv = (pll_con0 >> PLL36XX_MDIV_SHIFT) & PLL36XX_MDIV_MASK;
 	pdiv = (pll_con0 >> PLL36XX_PDIV_SHIFT) & PLL36XX_PDIV_MASK;
 	sdiv = (pll_con0 >> PLL36XX_SDIV_SHIFT) & PLL36XX_SDIV_MASK;
-	kdiv = pll_con1 & PLL36XX_KDIV_MASK;
+	kdiv = (s16)(pll_con1 & PLL36XX_KDIV_MASK);
 
 	fvco *= (mdiv << 16) + kdiv;
 	do_div(fvco, (pdiv << sdiv));
