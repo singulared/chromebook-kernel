2f0598f650a36c6ea61ed9159d4225b7d9ae31fc
Paulo Zanoni <paulo.r.zanoni@intel.com>
2013-05-31 17:36:55 -0700

    BACKPORT: drm/i915: add haswell_update_sprite_wm
    On Haswell, whenever we change the sprites we need to completely
    recalculate all the watermarks, because the sprites are one of the
    parameters to the LP watermarks, so a change on the sprites may
    trigger a change on which LP levels are enabled.
    
    So on this commit we store all the parameters we need to store for
    proper recalculation of the Haswell WMs and then call
    haswell_update_wm.
    
    Notice that for now our haswell_update_wm function is not really using
    these parameters we're storing, but on the next commits we'll use
    these parameters.
    
    Signed-off-by: Paulo Zanoni <paulo.r.zanoni@intel.com>
    Reviewed-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    (cherry picked from commit 526682e9fabf22e82a02383e8f864a7330b73b25)
    
    Conflicts:
    	drivers/gpu/drm/i915/intel_drv.h
            (context-only changes)
    
    Original-Change-Id: Ia4adcb596cbe0be9c1d3536d471bbcbcc8b8b824
    Change-Id: Ifcbbe5185a46930c8144bbaa848ea96fa3bf1ed4
    Reviewed-on: https://gerrit.chromium.org/gerrit/57331
    Tested-by: Jim Bride <jim.bride@intel.com>
    Commit-Queue: Stéphane Marchesin <marcheu@chromium.org>
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>

--- drivers/gpu/drm/i915/intel_drv.h
+++ drivers/gpu/drm/i915/intel_drv.h
@@ -269,6 +269,18 @@ struct intel_plane {
 	bool can_scale;
 	int max_downscale;
 	u32 lut_r[1024], lut_g[1024], lut_b[1024];
+
+	/* Since we need to change the watermarks before/after
+	 * enabling/disabling the planes, we need to store the parameters here
+	 * as the other pieces of the struct may not reflect the values we want
+	 * for the watermark calculations. Currently only Haswell uses this.
+	 */
+	struct {
+		bool enable;
+		uint8_t bytes_per_pixel;
+		uint32_t horiz_pixels;
+	} wm;
+
 	void (*update_plane)(struct drm_plane *plane,
 			     struct drm_framebuffer *fb,
 			     struct drm_i915_gem_object *obj,
