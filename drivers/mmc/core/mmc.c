65d9a898e8d9c300a5bdad1d86333c721616f38e
Ulf Hansson <ulf.hansson@linaro.org>
2013-06-10 17:03:44 +0200

    UPSTREAM: mmc: core: Add shutdown callback for (e)MMC bus_ops
    The shutdown sequence of an (e)MMC is very similar to a suspend. We
    re-use the suspend function and tell it we are not in suspend context.
    
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    Signed-off-by: Chris Ball <cjb@laptop.org>
    (cherry picked from commit 486fdbbc1483504cde0314e33e47bc32e9d38186)
    
    BUG=chromium:383481
    TEST=run bvt-inline and bvt-cq suite on spring
    
    Change-Id: I9223464b4020be6851626cb1df0e9ea0eeef9f60
    Reviewed-on: https://chromium-review.googlesource.com/212099
    Reviewed-by: Andrew Bresticker <abrestic@chromium.org>
    Tested-by: Puthikorn Voravootivat <puthik@chromium.org>

--- drivers/mmc/core/mmc.c
+++ drivers/mmc/core/mmc.c
@@ -1458,6 +1458,14 @@ static int mmc_suspend(struct mmc_host *host)
 }
 
 /*
+ * Shutdown callback
+ */
+static int mmc_shutdown(struct mmc_host *host)
+{
+	return _mmc_suspend(host, false);
+}
+
+/*
  * Resume callback from host.
  *
  * This function tries to determine if the same card is still present
@@ -1546,6 +1554,7 @@ static const struct mmc_bus_ops mmc_ops = {
 	.resume = NULL,
 	.power_restore = mmc_power_restore,
 	.alive = mmc_alive,
+	.shutdown = mmc_shutdown,
 };
 
 static const struct mmc_bus_ops mmc_ops_unsafe = {
@@ -1557,6 +1566,7 @@ static const struct mmc_bus_ops mmc_ops_unsafe = {
 	.runtime_resume = mmc_runtime_resume,
 	.power_restore = mmc_power_restore,
 	.alive = mmc_alive,
+	.shutdown = mmc_shutdown,
 };
 
 static void mmc_attach_bus_ops(struct mmc_host *host)
