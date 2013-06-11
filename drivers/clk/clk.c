84141f62dff51652d5c8e43a3829065b1a994334
Andrew Bresticker <abrestic@chromium.org>
2013-06-11 09:57:09 -0700

    clk: add missing clk_prepare_unlock() in clk_set_rate()
    In the non-error case, clk_set_rate() unlocks prepare_lock directly
    instead of using clk_prepare_unlock().  Because of this, prepare_owner
    and prepare_refcnt are not properly updated, triggering WARN_ONs in the
    next clk_prepare_lock().  Fix clk_set_rate() so that clk_prepare_unlock()
    is always called.
    
    BUG=chrome-os-partner:20085
    TEST=No more warnings in clk_prepare_lock() when booting on peach-pit.
    
    Change-Id: I6144bb7a3996b04495a45ad037680136eb792ae6
    Signed-off-by: Andrew Bresticker <abrestic@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/58204
    Reviewed-by: Simon Glass <sjg@chromium.org>

--- drivers/clk/clk.c
+++ drivers/clk/clk.c
@@ -1155,10 +1155,6 @@ int clk_set_rate(struct clk *clk, unsigned long rate)
 
 	/* change the rates */
 	clk_change_rate(top);
-
-	mutex_unlock(&prepare_lock);
-
-	return 0;
 out:
 	clk_prepare_unlock();
 
