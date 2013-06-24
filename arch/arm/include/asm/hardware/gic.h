2513d598ee5553e627f495630a21336c372b6f49
Nicolas Pitre <nicolas.pitre@linaro.org>
2013-06-24 14:47:30 +0530

    ARM: GIC: function to retrieve the physical address of the SGIR
    In order to have early assembly code signal other CPUs in the system,
    we need to get the physical address for the SGIR register used to
    send IPIs.  Because the register will be used with a precomputed CPU
    interface ID number, there is no need for any locking in the assembly
    code where this register is written to.
    
    Signed-off-by: Nicolas Pitre <nico@linaro.org>
    (cherry-picked from commit df321c43e3c3ed44d2434fd12d378736a82152d5)
    (cherry-picked from: git://git.linaro.org/people/nico/linux.git)
    
    BUG=chrome-os-partner:19820
    TEST=Booted peach
    
    Change-Id: I9c0260a9285f7e99fbe23540afb48f6049d4a29f
    Signed-off-by: Abhilash Kesavan <a.kesavan@samsung.com>
    [abrestic: fixed style errors]
    Signed-off-by: Andrew Bresticker <abrestic@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/59036

--- arch/arm/include/asm/hardware/gic.h
+++ arch/arm/include/asm/hardware/gic.h
@@ -41,6 +41,7 @@ struct device_node;
 extern struct irq_chip gic_arch_extn;
 int gic_get_cpu_id(unsigned int cpu);
 void gic_migrate_target(unsigned int new_cpu_id);
+unsigned long gic_get_sgir_physaddr(void);
 
 void gic_init_bases(unsigned int, int, void __iomem *, void __iomem *,
 		    u32 offset, struct device_node *);
