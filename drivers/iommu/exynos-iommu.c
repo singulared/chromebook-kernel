4c27a44a835b1f86b945779d19a720c63b0a40db
Prathyush K <prathyush.k@samsung.com>
2013-07-05 11:09:54 +0530

    CHROMIUM: iommu/exynos: fix runtime pm functionality
    Modify the pm functionality of the iommu driver to ensure that TLBs are
    not invalidated when the sysmmu is turned off. Also, ensure that sysmmu
    does not get enabled or disabled twice.
    
    BUG=chromium:219207
    TEST=build/boot. run Video and S2R on snow.
    
    Change-Id: Ifaaae536ae084f254014e94a400a6f6d415525f7
    Signed-off-by: Prathyush K <prathyush.k@samsung.com>
    Reviewed-on: https://gerrit.chromium.org/gerrit/61013
    Tested-by: Grant Grundler <grundler@chromium.org>
    Reviewed-by: Prathyush Kalashwaram <prathyush@chromium.org>
    Commit-Queue: Grant Grundler <grundler@chromium.org>
    Reviewed-by: Grant Grundler <grundler@chromium.org>

--- drivers/iommu/exynos-iommu.c
+++ drivers/iommu/exynos-iommu.c
@@ -176,6 +176,7 @@ struct sysmmu_drvdata {
 	struct iommu_domain *domain;
 	sysmmu_fault_handler_t fault_handler;
 	unsigned long pgtable;
+	bool runtime_active;
 };
 
 static bool set_sysmmu_active(struct sysmmu_drvdata *data)
@@ -420,6 +421,7 @@ static void __exynos_sysmmu_enable(struct sysmmu_drvdata *data,
 	}
 
 	data->domain = domain;
+	data->runtime_active = true;
 
 	dev_dbg(data->sysmmu, "(%s) Enabled\n", data->dbgname);
 
@@ -470,7 +472,7 @@ static void sysmmu_tlb_invalidate_entry(struct device *dev, unsigned long iova)
 
 	read_lock_irqsave(&data->lock, flags);
 
-	if (is_sysmmu_active(data)) {
+	if (is_sysmmu_active(data) && data->runtime_active) {
 		int i;
 		for (i = 0; i < data->nsfrs; i++) {
 			if (sysmmu_block(data->sfrbases[i])) {
@@ -618,11 +620,12 @@ err_alloc:
 
 static int exynos_runtime_suspend(struct device *dev)
 {
-	struct sysmmu_drvdata *data;
-	data = dev_get_drvdata(dev);
+	struct sysmmu_drvdata *data = dev_get_drvdata(dev);
 
-	if (!is_sysmmu_active(data))
+	if (!is_sysmmu_active(data) || !data->runtime_active)
 		return 0;
+
+	data->runtime_active = false;
 
 	if (data->clk[1])
 		clk_disable(data->clk[1]);
@@ -633,21 +636,9 @@ static int exynos_runtime_suspend(struct device *dev)
 
 static int exynos_runtime_resume(struct device *dev)
 {
-	struct sysmmu_drvdata *data;
-	data = dev_get_drvdata(dev);
+	struct sysmmu_drvdata *data = dev_get_drvdata(dev);
 
-	if (is_sysmmu_active(data))
-		__exynos_sysmmu_enable(data, data->pgtable, NULL);
-	return 0;
-}
-
-static int exynos_pm_resume(struct device *dev)
-{
-	struct sysmmu_drvdata *data;
-
-	data = dev_get_drvdata(dev);
-
-	if (is_sysmmu_active(data))
+	if (is_sysmmu_active(data) && !data->runtime_active)
 		__exynos_sysmmu_enable(data, data->pgtable, NULL);
 
 	return 0;
@@ -656,7 +647,8 @@ static int exynos_pm_resume(struct device *dev)
 const struct dev_pm_ops exynos_pm_ops = {
 	.runtime_suspend = &exynos_runtime_suspend,
 	.runtime_resume = &exynos_runtime_resume,
-	.resume = &exynos_pm_resume,
+	.suspend = &exynos_runtime_suspend,
+	.resume = &exynos_runtime_resume,
 };
 
 #ifdef CONFIG_OF
