6dc7703f5c9176ba8c362eb23c460f59824ab5e1
avnd kiran <avnd.kiran@samsung.com>
2013-07-17 18:03:38 +0530

    CHROMIUM: s5p-mfc: set default MAX QP value for encoder
    Rate control when enabled, target bitrate is achieved
    through adaptively adjusting the QP between the min and
    max values set by the driver.
    
    Fow H264 encoder, fix the default MAX QP value to 51, which
    is the maximum value QP can take. Now, MFC starts with a QP
    set using V4L2_CID_MPEG_VIDEO_H264_I_FRAME_QP and adapts to
    achieve the target bitrate.
    
    similarly, default MAX QP values are modified for h263 and
    mpeg4 encoders
    
    BUG=chrome-os-partner:18019
    BUG=chrome-os-partner:21917
    TEST=build, run mfc encoder on CrOS pit
    
    Change-Id: I407db86d56883413544075f639bfa9562474d929
    Signed-off-by: avnd kiran <avnd.kiran@samsung.com>
    Reviewed-on: https://gerrit.chromium.org/gerrit/62344
    Reviewed-by: John Sheu <sheu@chromium.org>
    Commit-Queue: kiran <kiran@chromium.org>
    Tested-by: kiran <kiran@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/66085
    Reviewed-by: Pawel Osciak <posciak@chromium.org>
    Commit-Queue: Pawel Osciak <posciak@chromium.org>

--- drivers/media/platform/s5p-mfc/s5p_mfc_enc.c
+++ drivers/media/platform/s5p-mfc/s5p_mfc_enc.c
@@ -356,7 +356,7 @@ static struct mfc_control controls[] = {
 		.minimum = 0,
 		.maximum = 51,
 		.step = 1,
-		.default_value = 1,
+		.default_value = 51,
 	},
 	{
 		.id = V4L2_CID_MPEG_VIDEO_H264_P_FRAME_QP,
@@ -399,7 +399,7 @@ static struct mfc_control controls[] = {
 		.minimum = 1,
 		.maximum = 31,
 		.step = 1,
-		.default_value = 1,
+		.default_value = 31,
 	},
 	{
 		.id = V4L2_CID_MPEG_VIDEO_H263_P_FRAME_QP,
@@ -444,7 +444,7 @@ static struct mfc_control controls[] = {
 		.minimum = 0,
 		.maximum = 51,
 		.step = 1,
-		.default_value = 1,
+		.default_value = 51,
 	},
 	{
 		.id = V4L2_CID_MPEG_VIDEO_MPEG4_P_FRAME_QP,
