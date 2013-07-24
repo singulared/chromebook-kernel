363aa388dd8b1ad23facb5c27f61db2be502986a
Takashi Iwai <tiwai@suse.de>
2013-07-24 14:31:50 +0200

    UPSTREAM: ALSA: hda/realtek - Selectively call snd_hda_shutup_pins()
    Instead of calling snd_hda_shutup_pins() unconditionally, allow it be
    called in spec->shutup callback.  In this way, we can avoid calling
    this function if it causes a problem like we see in the next patch
    following this.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    (cherry picked from commit 9bfb2844a2f9e6eab52aed1eca0d03f4398c755f)
    
    Change-Id: Iab39fcdf29f178794871a2e3eec20cfd88b86213
    Reviewed-on: https://gerrit.chromium.org/gerrit/66433
    Reviewed-by: Chih-Chung Chang <chihchung@chromium.org>
    Tested-by: Dylan Reid <dgreid@chromium.org>
    Commit-Queue: Dylan Reid <dgreid@chromium.org>

--- sound/pci/hda/patch_realtek.c
+++ sound/pci/hda/patch_realtek.c
@@ -265,6 +265,7 @@ static void alc_eapd_shutup(struct hda_codec *codec)
 {
 	alc_auto_setup_eapd(codec, false);
 	msleep(200);
+	snd_hda_shutup_pins(codec);
 }
 
 /* generic EAPD initialization */
@@ -809,7 +810,8 @@ static inline void alc_shutup(struct hda_codec *codec)
 
 	if (spec && spec->shutup)
 		spec->shutup(codec);
-	snd_hda_shutup_pins(codec);
+	else
+		snd_hda_shutup_pins(codec);
 }
 
 static void alc_free(struct hda_codec *codec)
@@ -2569,15 +2571,13 @@ static void alc269_shutup(struct hda_codec *codec)
 {
 	struct alc_spec *spec = codec->spec;
 
-	if (spec->codec_variant != ALC269_TYPE_ALC269VB)
-		return;
-
 	if (spec->codec_variant == ALC269_TYPE_ALC269VB)
 		alc269vb_toggle_power_output(codec, 0);
 	if (spec->codec_variant == ALC269_TYPE_ALC269VB &&
 			(alc_get_coef0(codec) & 0x00ff) == 0x018) {
 		msleep(150);
 	}
+	snd_hda_shutup_pins(codec);
 }
 
 #ifdef CONFIG_PM
