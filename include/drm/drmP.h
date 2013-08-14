c34e4942b971dc11951831d84771777c434d384a
Daniel Vetter <daniel.vetter@ffwll.ch>
2013-08-15 00:02:49 +0200

    drm/prime: Always add exported buffers to the handle cache
    ... not only when the dma-buf is freshly created. In contrived
    examples someone else could have exported/imported the dma-buf already
    and handed us the gem object with a flink name. If such on object gets
    reexported as a dma_buf we won't have it in the handle cache already,
    which breaks the guarantee that for dma-buf imports we always hand
    back an existing handle if there is one.
    
    This is exercised by igt/prime_self_import/with_one_bo_two_files
    
    Now if we extend the locked sections just a notch more we can also
    plug th racy buf/handle cache setup in handle_to_fd:
    
    If evil userspace races a concurrent gem close against a prime export
    operation we can end up tearing down the gem handle before the dma buf
    handle cache is set up. When handle_to_fd gets around to adding the
    handle to the cache there will be no one left to clean it up,
    effectily leaking the bo (and the dma-buf, since the handle cache
    holds a ref on the dma-buf):
    
    Thread A			Thread B
    
    handle_to_fd:
    
    lookup gem object from handle
    creates new dma_buf
    
    				gem_close on the same handle
    				obj->dma_buf is set, but file priv buf
    				handle cache has no entry
    
    				obj->handle_count drops to 0
    
    drm_prime_add_buf_handle sets up the handle cache
    
    -> We have a dma-buf reference in the handle cache, but since the
    handle_count of the gem object already dropped to 0 no on will clean
    it up. When closing the drm device fd we'll hit the WARN_ON in
    drm_prime_destroy_file_private.
    
    The important change is to extend the critical section of the
    filp->prime.lock to cover the gem handle lookup. This serializes with
    a concurrent gem handle close.
    
    This leak is exercised by igt/prime_self_import/export-vs-gem_close-race
    
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Signed-off-by: Dave Airlie <airlied@redhat.com>
    (cherry picked from commit d0b2c5334f41bdd18adaa3fbc1f7b5f1daab7eac)
    Signed-off-by: Stéphane Marchesin <marcheu@chromium.org>
    
    Conflicts:
    	drivers/gpu/drm/drm_prime.c
    
    BUG=chromium:340541
    TEST=builds and runs on link and daisy
    
    Change-Id: I98fc5214fa66ce2d1b8083c19936d658d50edcda
    Reviewed-on: https://chromium-review.googlesource.com/190651
    Tested-by: Stéphane Marchesin <marcheu@chromium.org>
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>
    Commit-Queue: Stéphane Marchesin <marcheu@chromium.org>

--- include/drm/drmP.h
+++ include/drm/drmP.h
@@ -1701,7 +1701,7 @@ int drm_gem_dumb_destroy(struct drm_file *file,
 
 void drm_prime_init_file_private(struct drm_prime_file_private *prime_fpriv);
 void drm_prime_destroy_file_private(struct drm_prime_file_private *prime_fpriv);
-void drm_prime_remove_buf_handle(struct drm_prime_file_private *prime_fpriv, struct dma_buf *dma_buf);
+void drm_prime_remove_buf_handle_locked(struct drm_prime_file_private *prime_fpriv, struct dma_buf *dma_buf);
 
 int drm_prime_add_dma_buf(struct drm_device *dev, struct drm_gem_object *obj);
 int drm_prime_lookup_obj(struct drm_device *dev, struct dma_buf *buf,
