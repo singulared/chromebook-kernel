6d1466436445a8ead16fe7685f48ae04613eab81
Doug Anderson <dianders@chromium.org>
2013-08-20 15:01:58 -0700

    samsung: pm-check: Cleanup chunk exclude code to use suspend_volatile
    Recent changes mark memory that may change across suspend/resume
    as suspend_volatile.  We can now use this in pm-check, so do so.
    That means we can remove some extra globals from printk.c.
    
    This change also cleans up handling of sleep_save_sp:
    * Don't exclude *sleep_save_sp.  That was a physical address and is
      also already handled by current_thread_info().
    * Don't exclude sleep_save_sp. Instead: we zero it out before out
      code runs.  This is a transitory value and zeroing it is fine.
      Now we won't exclude chunks of memory stored near sleep_save_sp.
    
    ...and a few other minor fixes:
    * Skips regions in s3c_pm_makecheck() too.
    * Avoids goto in s3c_pm_runcheck() loop.
    
    Update the exclusions to ignore log_buf and sleep_save_sp by annotating
    them with suspend_volatile
    
    BUG=chromium-os:267716
    TEST=Build and boot snow
    
    Change-Id: Ib29e3d04fc63d08494911d3b3a1f65ec042cb9e8
    Signed-off-by: Doug Anderson <dianders@chromium.org>
    Signed-off-by: Olof Johansson <olofj@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/171889

--- arch/arm/plat-samsung/pm-check.c
+++ arch/arm/plat-samsung/pm-check.c
@@ -13,10 +13,16 @@
 */
 
 #include <linux/kernel.h>
+
+#include <linux/addr_overlap.h>
 #include <linux/suspend.h>
 #include <linux/init.h>
 #include <linux/crc32.h>
+#include <linux/highmem.h>
 #include <linux/ioport.h>
+#include <asm/memory.h>
+#include <linux/module.h>
+#include <linux/moduleparam.h>
 #include <linux/slab.h>
 
 #include <plat/pm.h>
@@ -35,12 +41,97 @@
  * and reducing the size will cause the CRC save area to grow
 */
 
-#define CHECK_CHUNKSIZE (CONFIG_SAMSUNG_PM_CHECK_CHUNKSIZE * 1024)
+static bool pm_check_use_xor = 1;
+static bool pm_check_enabled;
+static bool pm_check_should_panic;
+static bool pm_check_print_skips;
+static bool pm_check_print_timings;
+static int pm_check_chunksize = CONFIG_SAMSUNG_PM_CHECK_CHUNKSIZE * 1024;
 
 static u32 crc_size;	/* size needed for the crc block */
 static u32 *crcs;	/* allocated over suspend/resume */
 
+/* number of errors found this resume */
+static __suspend_volatile_bss u32 crc_err_cnt;
+
+
 typedef u32 *(run_fn_t)(struct resource *ptr, u32 *arg);
+
+module_param(pm_check_use_xor, bool, S_IRUGO | S_IWUSR);
+MODULE_PARM_DESC(pm_check_use_xor, "Use XOR checks instead of CRC");
+module_param(pm_check_enabled, bool, S_IRUGO | S_IWUSR);
+MODULE_PARM_DESC(pm_check_enabled,
+		"Enable memory validation on suspend/resume");
+module_param(pm_check_should_panic, bool, S_IRUGO | S_IWUSR);
+MODULE_PARM_DESC(pm_check_should_panic, "Panic on CRC errors");
+module_param(pm_check_print_skips, bool, S_IRUGO | S_IWUSR);
+MODULE_PARM_DESC(pm_check_print_skips, "Print skipped regions");
+module_param(pm_check_print_timings, bool, S_IRUGO | S_IWUSR);
+MODULE_PARM_DESC(pm_check_print_timings,
+		"Print time to compute checks (causes runtime warnings)");
+module_param(pm_check_chunksize, int, S_IRUGO | S_IWUSR);
+MODULE_PARM_DESC(pm_check_chunksize,
+		"Size of blocks for CRCs, shold be 1K multiples");
+
+/**
+ * s3c_pm_xor_mem() - XOR all the words in the memory range passed
+ * @val: Initial value to start the XOR from
+ * @ptr: Pointer to the start of the memory range
+ * @len: Length (in bytes) of the memory range
+ *
+ */
+static u32 s3c_pm_xor_mem(u32 val, const u32 *ptr, size_t len)
+{
+	int i;
+
+	len >>= 2; /* get words, not bytes */
+	for (i = 0; i < len; i++, ptr++)
+		val ^= *ptr;
+	return val;
+}
+
+/**
+ * s3c_pm_process_mem() - Process one memory chunk for checksum
+ * @val: Starting point for checksum function
+ * @addr: Pointer to the physical memory range.  This must be page-aligned.
+ * @len: Length (in bytes) of the memory range
+ *
+ * Loop through the chunk in PAGE_SIZE blocks, ensuring each
+ * page is mapped.  Use either crc32 or xor checksum and return
+ * back.
+ */
+static inline u32 s3c_pm_process_mem(u32 val, u32 addr, size_t len)
+{
+	size_t processed = 0;
+
+	if (unlikely(addr & (PAGE_SIZE - 1))) {
+		printk(KERN_ERR "s3c_pm_check: Unaligned address: 0x%x\n",
+			addr);
+		return val;
+	}
+	while (processed < len) {
+		int pfn = __phys_to_pfn(addr + processed);
+		unsigned long left = len - processed;
+		void *virt;
+
+		if (left > PAGE_SIZE)
+			left = PAGE_SIZE;
+		virt = kmap_atomic(pfn_to_page(pfn));
+		if (!virt) {
+			printk(KERN_ERR "s3c_pm_check: Could not map "
+				"page for pfn %d with addr 0x%x\n",
+				pfn, addr + len - processed);
+			return val;
+		}
+		if (pm_check_use_xor)
+			val = s3c_pm_xor_mem(val, virt, left);
+		else
+			val = crc32_le(val, virt, left);
+		kunmap_atomic(virt);
+		processed += left;
+	}
+	return val;
+}
 
 /* s3c_pm_run_res
  *
@@ -55,7 +146,8 @@ static void s3c_pm_run_res(struct resource *ptr, run_fn_t fn, u32 *arg)
 
 		if ((ptr->flags & IORESOURCE_MEM) &&
 		    strcmp(ptr->name, "System RAM") == 0) {
-			S3C_PMDBG("Found system RAM at %08lx..%08lx\n",
+			S3C_PMDBG("s3c_pm_check: Found system RAM at "
+				  "%08lx..%08lx\n",
 				  (unsigned long)ptr->start,
 				  (unsigned long)ptr->end);
 			arg = (fn)(ptr, arg);
@@ -74,10 +166,9 @@ static u32 *s3c_pm_countram(struct resource *res, u32 *val)
 {
 	u32 size = (u32)resource_size(res);
 
-	size += CHECK_CHUNKSIZE-1;
-	size /= CHECK_CHUNKSIZE;
+	size = DIV_ROUND_UP(size, pm_check_chunksize);
 
-	S3C_PMDBG("Area %08lx..%08lx, %d blocks\n",
+	S3C_PMDBG("s3c_pm_check: Area %08lx..%08lx, %d blocks\n",
 		  (unsigned long)res->start, (unsigned long)res->end, size);
 
 	*val += size * sizeof(u32);
@@ -91,33 +182,92 @@ static u32 *s3c_pm_countram(struct resource *res, u32 *val)
  * allocating, and thus touching bits of the kernel we do not
  * know about.
 */
-
 void s3c_pm_check_prepare(void)
 {
+	ktime_t start, stop, delta;
+
+	if (!pm_check_enabled)
+		return;
+
 	crc_size = 0;
-
+	/* Timing code generates warnings at this point in suspend */
+	if (pm_check_print_timings)
+		start = ktime_get();
 	s3c_pm_run_sysram(s3c_pm_countram, &crc_size);
+	if (pm_check_print_timings) {
+		stop = ktime_get();
+		delta = ktime_sub(stop, start);
+		S3C_PMDBG("s3c_pm_check: Suspend prescan took %lld usecs\n",
+			(unsigned long long)ktime_to_ns(delta) >> 10);
+	}
 
-	S3C_PMDBG("s3c_pm_prepare_check: %u checks needed\n", crc_size);
+	S3C_PMDBG("s3c_pm_check: Chunk size: %d, %u bytes for checks needed\n",
+		pm_check_chunksize, crc_size);
 
 	crcs = kmalloc(crc_size+4, GFP_KERNEL);
 	if (crcs == NULL)
 		printk(KERN_ERR "Cannot allocated CRC save area\n");
 }
 
+static inline void s3c_pm_printskip(char *desc, unsigned long addr)
+{
+	if (!pm_check_print_skips)
+		return;
+	S3C_PMDBG("s3c_pm_check: skipping %08lx, has %s in\n", addr, desc);
+}
+
+/* externs from sleep.S */
+extern u32 *sleep_save_sp;
+
+/**
+ * Zero memory that the sleep code may have used but doesn't care about.
+ *
+ * The sleep code uses some temporary memory that it doesn't need anymore
+ * after resume.  We zero it out here so we don't have to skip a whole chunk.
+ * This keeps CRCs consistent.
+ *
+ * Alternatively we could change sleep code to zero this memory, but we run
+ * into some issues if we ever get a failed suspend (the normal resume code
+ * doesn't run).
+ */
+static void zero_temp_memory(void)
+{
+	sleep_save_sp = 0;
+}
+
+static bool s3c_pm_should_skip(phys_addr_t addr, u32 size)
+{
+	void *stkbase = current_thread_info();
+
+	if (phys_addrs_overlap(addr, size,
+			       virt_to_phys(stkbase), THREAD_SIZE)) {
+		s3c_pm_printskip("stack", addr);
+		return true;
+	}
+	if (phys_addrs_overlap(addr, size, virt_to_phys(crcs), crc_size)) {
+		s3c_pm_printskip("crc block", addr);
+		return true;
+	}
+	if (pm_does_overlap_suspend_volatile(addr, size)) {
+		s3c_pm_printskip("volatile suspend data", addr);
+		return true;
+	}
+
+	return false;
+}
+
 static u32 *s3c_pm_makecheck(struct resource *res, u32 *val)
 {
 	unsigned long addr, left;
 
-	for (addr = res->start; addr < res->end;
-	     addr += CHECK_CHUNKSIZE) {
-		left = res->end - addr;
+	for (addr = res->start; addr <= res->end;
+	     addr += pm_check_chunksize, val++) {
+		left = res->end - addr + 1;
+		if (left > pm_check_chunksize)
+			left = pm_check_chunksize;
 
-		if (left > CHECK_CHUNKSIZE)
-			left = CHECK_CHUNKSIZE;
-
-		*val = crc32_le(~0, phys_to_virt(addr), left);
-		val++;
+		if (!s3c_pm_should_skip(addr, left))
+			*val = s3c_pm_process_mem(~0, addr, left);
 	}
 
 	return val;
@@ -131,25 +281,22 @@ static u32 *s3c_pm_makecheck(struct resource *res, u32 *val)
 
 void s3c_pm_check_store(void)
 {
-	if (crcs != NULL)
-		s3c_pm_run_sysram(s3c_pm_makecheck, crcs);
-}
+	ktime_t start, stop, delta;
 
-/* in_region
- *
- * return TRUE if the area defined by ptr..ptr+size contains the
- * what..what+whatsz
-*/
+	if (crcs == NULL)
+		return;
 
-static inline int in_region(void *ptr, int size, void *what, size_t whatsz)
-{
-	if ((what+whatsz) < ptr)
-		return 0;
+	if (pm_check_print_timings)
+		start = ktime_get();
 
-	if (what > (ptr+size))
-		return 0;
-
-	return 1;
+	zero_temp_memory();
+	s3c_pm_run_sysram(s3c_pm_makecheck, crcs);
+	if (pm_check_print_timings) {
+		stop = ktime_get();
+		delta = ktime_sub(stop, start);
+		S3C_PMDBG("s3c_pm_check: Suspend memory scan took %lld usecs\n",
+			(unsigned long long)ktime_to_ns(delta) >> 10);
+	}
 }
 
 /**
@@ -166,44 +313,27 @@ static u32 *s3c_pm_runcheck(struct resource *res, u32 *val)
 {
 	unsigned long addr;
 	unsigned long left;
-	void *stkpage;
-	void *ptr;
 	u32 calc;
 
-	stkpage = (void *)((u32)&calc & ~PAGE_MASK);
+	for (addr = res->start; addr <= res->end;
+	     addr += pm_check_chunksize, val++) {
+		left = res->end - addr + 1;
 
-	for (addr = res->start; addr < res->end;
-	     addr += CHECK_CHUNKSIZE) {
-		left = res->end - addr;
+		if (left > pm_check_chunksize)
+			left = pm_check_chunksize;
 
-		if (left > CHECK_CHUNKSIZE)
-			left = CHECK_CHUNKSIZE;
-
-		ptr = phys_to_virt(addr);
-
-		if (in_region(ptr, left, stkpage, 4096)) {
-			S3C_PMDBG("skipping %08lx, has stack in\n", addr);
-			goto skip_check;
-		}
-
-		if (in_region(ptr, left, crcs, crc_size)) {
-			S3C_PMDBG("skipping %08lx, has crc block in\n", addr);
-			goto skip_check;
-		}
+		if (s3c_pm_should_skip(addr, left))
+			continue;
 
 		/* calculate and check the checksum */
 
-		calc = crc32_le(~0, ptr, left);
+		calc = s3c_pm_process_mem(~0, addr, left);
 		if (calc != *val) {
-			printk(KERN_ERR "Restore CRC error at "
-			       "%08lx (%08x vs %08x)\n", addr, calc, *val);
-
-			S3C_PMDBG("Restore CRC error at %08lx (%08x vs %08x)\n",
-			    addr, calc, *val);
+			crc_err_cnt++;
+			S3C_PMDBG("s3c_pm_check: Restore CRC error at %08lx "
+				"(%08x vs %08x)\n",
+				addr, calc, *val);
 		}
-
-	skip_check:
-		val++;
 	}
 
 	return val;
@@ -217,8 +347,27 @@ static u32 *s3c_pm_runcheck(struct resource *res, u32 *val)
 */
 void s3c_pm_check_restore(void)
 {
-	if (crcs != NULL)
-		s3c_pm_run_sysram(s3c_pm_runcheck, crcs);
+	ktime_t start, stop, delta;
+
+	if (crcs == NULL)
+		return;
+
+	crc_err_cnt = 0;
+	if (pm_check_print_timings)
+		start = ktime_get();
+	zero_temp_memory();
+	s3c_pm_run_sysram(s3c_pm_runcheck, crcs);
+	if (pm_check_print_timings) {
+		stop = ktime_get();
+		delta = ktime_sub(stop, start);
+		S3C_PMDBG("s3c_pm_check: Resume memory scan took %lld usecs\n",
+			(unsigned long long)ktime_to_ns(delta) >> 10);
+	}
+	if (crc_err_cnt) {
+		S3C_PMDBG("s3c_pm_check: %d CRC errors\n", crc_err_cnt);
+		if (pm_check_should_panic)
+			panic("%d CRC errors\n", crc_err_cnt);
+	}
 }
 
 /**
@@ -235,3 +384,12 @@ void s3c_pm_check_cleanup(void)
 	crcs = NULL;
 }
 
+/**
+ * s3c_pm_check_enable() - enable suspend/resume memory checks
+ * @enabled: True to enable, false to disable
+ *
+ */
+void s3c_pm_check_set_enable(bool enabled)
+{
+	pm_check_enabled = enabled;
+}
--- kernel/printk.c
+++ kernel/printk.c
@@ -250,7 +250,7 @@ static u32 clear_idx;
 #define LOG_ALIGN __alignof__(struct log)
 #endif
 #define __LOG_BUF_LEN (1 << CONFIG_LOG_BUF_SHIFT)
-static char __log_buf[__LOG_BUF_LEN] __aligned(LOG_ALIGN);
+static __suspend_volatile_bss char __log_buf[__LOG_BUF_LEN] __aligned(LOG_ALIGN);
 static char *log_buf = __log_buf;
 static u32 log_buf_len = __LOG_BUF_LEN;
 
