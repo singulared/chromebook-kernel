312fa31d702c9ed3e80bd6b45d6c1d529199432e
Kiran AVND <avnd.kiran@samsung.com>
2013-08-23 18:57:17 +0530

    CHROMIUM: s5p-mfc: fix encoder bitrate deviation
    when Rate Control is enabled, mfc tends to
    deviate far away from user requested bitrate.
    
    default GOP size should be 12 for mfc encoding.
    This is as per v4l2 controls documentation
    Documentation/DocBook/media/v4l/controls.xml
    
    BUG=chrome-os-partner:18019
    TEST=local build, run on CrOS pit
    test encoder for various bitrates and check if we get so.
    
    Change-Id: I9f60df1d0d3f53f47f610614f4619a444b681ff1
    Signed-off-by: Kiran AVND <avnd.kiran@samsung.com>
    Reviewed-on: https://gerrit.chromium.org/gerrit/66805
    Tested-by: kiran <kiran@chromium.org>
    Reviewed-by: Pawel Osciak <posciak@chromium.org>
    Commit-Queue: Pawel Osciak <posciak@chromium.org>

--- drivers/media/platform/s5p-mfc/s5p_mfc_enc.c
+++ drivers/media/platform/s5p-mfc/s5p_mfc_enc.c
@@ -113,7 +113,7 @@ static struct mfc_control controls[] = {
 		.minimum = 0,
 		.maximum = (1 << 16) - 1,
 		.step = 1,
-		.default_value = 0,
+		.default_value = 12,
 	},
 	{
 		.id = V4L2_CID_MPEG_VIDEO_MULTI_SLICE_MODE,
