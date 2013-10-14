50931d372af801c2ccd5cc767b8f071fcc7dacc2
Doug Anderson <dianders@chromium.org>
2013-10-14 09:30:19 -0700

    mmc: dw_mmc: Protect read-modify-write of INTMASK with a lock
    We're running into cases where our enabling of the SDIO interrupt in
    dw_mmc doesn't actually take effect.  Specifically, adding patch like
    this:
    
     +++ b/drivers/mmc/host/dw_mmc.c
     @@ -1076,6 +1076,9 @@ static void dw_mci_enable_sdio_irq(struct mmc_host *mmc, int enb)
    
          mci_writel(host, INTMASK,
               (int_mask | SDMMC_INT_SDIO(slot->id)));
     +    int_mask = mci_readl(host, INTMASK);
     +    if (!(int_mask & SDMMC_INT_SDIO(slot->id)))
     +      dev_err(&mmc->class_dev, "failed to enable sdio irq\n");
        } else {
    
    ...actually triggers the error message.  That's because the
    dw_mci_enable_sdio_irq() unsafely does a read-modify-write of the
    INTMASK register.
    
    We can't just use the standard host->lock since that lock is not irq
    safe and mmc_signal_sdio_irq() (called from interrupt context) calls
    dw_mci_enable_sdio_irq().  Add a new irq-safe lock to protect INTMASK.
    
    An alternate solution to this is to punt mmc_signal_sdio_irq() to the
    tasklet and then protect INTMASK modifications by the standard host
    lock.  This seemed like a bit more of a high-latency change.
    
    BUG=chrome-os-partner:22915
    TEST=Use Bluetooth and WiFi at the same time and see no problems.
    TEST=Cherry-pick I28250e40aba0fa901d6d5869dd6f77486d3412f7 and it works.
    
    Change-Id: I488c4bf028a6f7969a0c6d6fc6fcf3cd46bf87af
    Reported-by: Bing Zhao <bzhao@marvell.com>
    Signed-off-by: Doug Anderson <dianders@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/173132
    Reviewed-by: Sonny Rao <sonnyrao@chromium.org>
    Reviewed-by: Paul Stewart <pstew@chromium.org>
    Tested-by: Ashok Nagarajan <asnagarajan@chromium.org>

--- drivers/mmc/host/dw_mmc.c
+++ drivers/mmc/host/dw_mmc.c
@@ -565,6 +565,7 @@ static void dw_mci_post_req(struct mmc_host *mmc,
 
 static int dw_mci_submit_data_dma(struct dw_mci *host, struct mmc_data *data)
 {
+	unsigned long irqflags;
 	int sg_len;
 	u32 temp;
 
@@ -593,9 +594,11 @@ static int dw_mci_submit_data_dma(struct dw_mci *host, struct mmc_data *data)
 	mci_writel(host, CTRL, temp);
 
 	/* Disable RX/TX IRQs, let DMA handle it */
+	spin_lock_irqsave(&host->intmask_lock, irqflags);
 	temp = mci_readl(host, INTMASK);
 	temp  &= ~(SDMMC_INT_RXDR | SDMMC_INT_TXDR);
 	mci_writel(host, INTMASK, temp);
+	spin_unlock_irqrestore(&host->intmask_lock, irqflags);
 
 	host->dma_ops->start(host, sg_len);
 
@@ -604,6 +607,7 @@ static int dw_mci_submit_data_dma(struct dw_mci *host, struct mmc_data *data)
 
 static void dw_mci_submit_data(struct dw_mci *host, struct mmc_data *data)
 {
+	unsigned long irqflags;
 	u32 temp;
 
 	data->error = -EINPROGRESS;
@@ -640,9 +644,12 @@ static void dw_mci_submit_data(struct dw_mci *host, struct mmc_data *data)
 		host->part_buf_count = 0;
 
 		mci_writel(host, RINTSTS, SDMMC_INT_TXDR | SDMMC_INT_RXDR);
+
+		spin_lock_irqsave(&host->intmask_lock, irqflags);
 		temp = mci_readl(host, INTMASK);
 		temp |= SDMMC_INT_TXDR | SDMMC_INT_RXDR;
 		mci_writel(host, INTMASK, temp);
+		spin_unlock_irqrestore(&host->intmask_lock, irqflags);
 
 		temp = mci_readl(host, CTRL);
 		temp &= ~SDMMC_CTRL_DMA_ENABLE;
@@ -1075,7 +1082,10 @@ static void dw_mci_enable_sdio_irq(struct mmc_host *mmc, int enb)
 {
 	struct dw_mci_slot *slot = mmc_priv(mmc);
 	struct dw_mci *host = slot->host;
+	unsigned long irqflags;
 	u32 int_mask;
+
+	spin_lock_irqsave(&host->intmask_lock, irqflags);
 
 	/* Enable/disable Slot Specific SDIO interrupt */
 	int_mask = mci_readl(host, INTMASK);
@@ -1084,6 +1094,8 @@ static void dw_mci_enable_sdio_irq(struct mmc_host *mmc, int enb)
 	else
 		int_mask &= ~SDMMC_INT_SDIO(slot->id);
 	mci_writel(host, INTMASK, int_mask);
+
+	spin_unlock_irqrestore(&host->intmask_lock, irqflags);
 }
 
 /* initialize the clock sample to given value */
@@ -2553,6 +2565,7 @@ int dw_mci_probe(struct dw_mci *host)
 	host->quirks = host->pdata->quirks;
 
 	spin_lock_init(&host->lock);
+	spin_lock_init(&host->intmask_lock);
 	INIT_LIST_HEAD(&host->queue);
 
 	/*
