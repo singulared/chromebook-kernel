ab96a12cf5d5ea499b3c67ac80421020bee60652
Jesse Barnes <jbarnes@virtuousgeek.org>
2013-11-26 09:13:41 -0800

    CHROMIUM: drm/i915: take mode config lock around crtc disable at suspend
    This is just a theoretical issue, but we need to do this to prevent the
    WARN in pipe_from_connector at suspend time.
    
    This regression has been introduce in
    
    commit 7bd688cd66db93f6430f6e2b3145ee5686daa315
    Author: Jani Nikula <jani.nikula@intel.com>
    Date:   Fri Nov 8 16:48:56 2013 +0200
    
        drm/i915: handle backlight through chip specific functions
    
    https://bugs.freedesktop.org/show_bug.cgi?id=71978
    Signed-off-by: Jesse Barnes <jbarnes@virtuousgeek.org>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Signed-off-by: Wayne Boyer <wayne.boyer@intel.com>
    
    BUG=chromium:577441
    TEST=suspend/resume on peppy/link
    
    Reviewed-on: https://chromium-review.googlesource.com/192471
    Reviewed-by: Stphane Marchesin <marcheu@chromium.org>
    Tested-by: Wayne Boyer <wayne.boyer@intel.com>
    Commit-Queue: Wayne Boyer <wayne.boyer@intel.com>
    (cherry picked from commit 8cfd4ba4ee8f60cf6d6af2d92becac5c7edf0576)
    Signed-off-by: Derek Basehore <dbasehore@chromium.org>
    
    Change-Id: I6e732e38244165e0eb75f55947479145173acef2
    Reviewed-on: https://chromium-review.googlesource.com/322642
    Commit-Ready: Derek Basehore <dbasehore@chromium.org>
    Tested-by: Derek Basehore <dbasehore@chromium.org>
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>
    Reviewed-by: Eric Caruso <ejcaruso@chromium.org>

--- drivers/gpu/drm/i915/i915_drv.c
+++ drivers/gpu/drm/i915/i915_drv.c
@@ -521,8 +521,10 @@ static int i915_drm_freeze(struct drm_device *dev)
 		 * Disable CRTCs directly since we want to preserve sw state
 		 * for _thaw.
 		 */
+		mutex_lock(&dev->mode_config.mutex);
 		list_for_each_entry(crtc, &dev->mode_config.crtc_list, head)
 			dev_priv->display.crtc_disable(crtc);
+		mutex_unlock(&dev->mode_config.mutex);
 	}
 
 	i915_save_state(dev);
