437b20cadf46eee843c984f19f670bc97cff6572
Arun Mankuzhi <arun.m@samsung.com>
2014-01-02 14:05:40 +0530

    Revert "CHROMIUM: ARM: SAMSUNG: Add residue DMA operation."
    This reverts commit 5e790819352b73ee8571fd6338b70cdc1ae836ac.
    
    CQ-DEPEND=CL:181358
    BUG=chrome-os-partner:24263
    TEST=Play Mv.html and check for zero frame drop
    
    Change-Id: I845200504596428a529a8c4f0b6833b45a127e73
    Reviewed-on: https://chromium-review.googlesource.com/181316
    Reviewed-by: Dylan Reid <dgreid@chromium.org>
    Tested-by: Dylan Reid <dgreid@chromium.org>
    Commit-Queue: Doug Anderson <dianders@chromium.org>

--- arch/arm/plat-samsung/include/plat/dma-ops.h
+++ arch/arm/plat-samsung/include/plat/dma-ops.h
@@ -47,7 +47,6 @@ struct samsung_dma_ops {
 	int (*started)(unsigned ch);
 	int (*flush)(unsigned ch);
 	int (*stop)(unsigned ch);
-	unsigned (*residue)(unsigned ch);
 };
 
 extern void *samsung_dmadev_get_ops(void);
