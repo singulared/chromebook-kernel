af25719d1bb66845e97be8bcdf37d9bdc6f87612
hayeswang <hayeswang@realtek.com>
2014-01-03 11:21:56 +0800

    r8152: fix the wrong return value
    The return value should be the boolean value, not the error code.
    
    Signed-off-by: Hayes Wang <hayeswang@realtek.com>
    Spotted-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 45ea3932e261af147187ceefb43456c7f8e3b288)
    
    BUG=chrome-os-partner:24570
    TEST=manual testing with RealTek (not Samsung) USB rtl8153
    
    Change-Id: Ia2bf0665c03f851b18958b9e188cea6283d83a51
    Signed-off-by: Grant Grundler <grundler@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/185457
    Reviewed-by: Julius Werner <jwerner@chromium.org>

--- drivers/net/usb/r8152.c
+++ drivers/net/usb/r8152.c
@@ -2709,7 +2709,7 @@ static bool rtl_ops_init(struct r8152 *tp, const struct usb_device_id *id)
 			ops->unload		= rtl8153_unload;
 			break;
 		default:
-			ret = -EFAULT;
+			ret = false;
 			break;
 		}
 		break;
