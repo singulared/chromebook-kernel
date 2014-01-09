3fa4945de2015a4646afba594cc477200824e6db
Maarten Lankhorst <maarten.lankhorst@canonical.com>
2014-01-09 11:03:13 +0100

    seqno-fence: Hardware dma-buf implementation of fencing (v4)
    This type of fence can be used with hardware synchronization for simple
    hardware that can block execution until the condition
    (dma_buf[offset] - value) >= 0 has been met.
    
    A software fallback still has to be provided in case the fence is used
    with a device that doesn't support this mechanism. It is useful to expose
    this for graphics cards that have an op to support this.
    
    Some cards like i915 can export those, but don't have an option to wait,
    so they need the software fallback.
    
    I extended the original patch by Rob Clark.
    
    v1: Original
    v2: Renamed from bikeshed to seqno, moved into dma-fence.c since
        not much was left of the file. Lots of documentation added.
    v3: Use fence_ops instead of custom callbacks. Moved to own file
        to avoid circular dependency between dma-buf.h and fence.h
    v4: Add spinlock pointer to seqno_fence_init
    
    Signed-off-by: Maarten Lankhorst <maarten.lankhorst@canonical.com>
    (cherry picked from commit 3f9d0298d8490091b8b5b763be429f73c8e4f179)
    Signed-off-by: Stéphane Marchesin <marcheu@chromium.org>
    
    Conflicts:
    	Documentation/DocBook/device-drivers.tmpl
    
    BUG=chromium:340541
    TEST=builds and runs on link and daisy
    
    Change-Id: I7663872671b77550fd1ae3320408f9f2ad263a73
    Reviewed-on: https://chromium-review.googlesource.com/190656
    Tested-by: Stéphane Marchesin <marcheu@chromium.org>
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>
    Commit-Queue: Stéphane Marchesin <marcheu@chromium.org>

--- drivers/base/fence.c
+++ drivers/base/fence.c
@@ -25,6 +25,7 @@
 #include <linux/export.h>
 #include <linux/atomic.h>
 #include <linux/fence.h>
+#include <linux/seqno-fence.h>
 
 #define CREATE_TRACE_POINTS
 #include <trace/events/fence.h>
@@ -413,3 +414,52 @@ __fence_init(struct fence *fence, const struct fence_ops *ops,
 	trace_fence_init(fence);
 }
 EXPORT_SYMBOL(__fence_init);
+
+static const char *seqno_fence_get_driver_name(struct fence *fence) {
+	struct seqno_fence *seqno_fence = to_seqno_fence(fence);
+	return seqno_fence->ops->get_driver_name(fence);
+}
+
+static const char *seqno_fence_get_timeline_name(struct fence *fence) {
+	struct seqno_fence *seqno_fence = to_seqno_fence(fence);
+	return seqno_fence->ops->get_timeline_name(fence);
+}
+
+static bool seqno_enable_signaling(struct fence *fence)
+{
+	struct seqno_fence *seqno_fence = to_seqno_fence(fence);
+	return seqno_fence->ops->enable_signaling(fence);
+}
+
+static bool seqno_signaled(struct fence *fence)
+{
+	struct seqno_fence *seqno_fence = to_seqno_fence(fence);
+	return seqno_fence->ops->signaled && seqno_fence->ops->signaled(fence);
+}
+
+static void seqno_release(struct fence *fence)
+{
+	struct seqno_fence *f = to_seqno_fence(fence);
+
+	dma_buf_put(f->sync_buf);
+	if (f->ops->release)
+		f->ops->release(fence);
+	else
+		kfree(f);
+}
+
+static long seqno_wait(struct fence *fence, bool intr, signed long timeout)
+{
+	struct seqno_fence *f = to_seqno_fence(fence);
+	return f->ops->wait(fence, intr, timeout);
+}
+
+const struct fence_ops seqno_fence_ops = {
+	.get_driver_name = seqno_fence_get_driver_name,
+	.get_timeline_name = seqno_fence_get_timeline_name,
+	.enable_signaling = seqno_enable_signaling,
+	.signaled = seqno_signaled,
+	.wait = seqno_wait,
+	.release = seqno_release,
+};
+EXPORT_SYMBOL(seqno_fence_ops);
