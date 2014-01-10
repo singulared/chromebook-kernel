d5af8c62fc7e53e6aebe82fee87ef30d8b88d825
John Sheu <sheu@google.com>
2014-01-10 15:03:17 -0800

    drm/exynos: Extrapolate vblank counts while vblank irq disabled
    Exynos does not have not have a hardware vblank counter for fimd, mixer,
    or vidi, so the drm vblank counter is not updated when vblank irqs are
    disabled.  Extrapolate the vblank counter for this case using time interval
    and frame timing.
    
    Signed-off-by: John Sheu <sheu@google.com>
    
    BUG=chromium:328953
    TEST=local build, run Chrome on CrOS snow
    
    Change-Id: I7a0da1eef18038a1c53927a04e7c771aadf6ca99
    Reviewed-on: https://chromium-review.googlesource.com/182219
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>
    Tested-by: John Sheu <sheu@chromium.org>
    Commit-Queue: John Sheu <sheu@chromium.org>
    Reviewed-by: Daniel Kurtz <djkurtz@chromium.org>

--- include/drm/drmP.h
+++ include/drm/drmP.h
@@ -1454,6 +1454,7 @@ extern int drm_vblank_get(struct drm_device *dev, int crtc);
 extern void drm_vblank_put(struct drm_device *dev, int crtc);
 extern void drm_vblank_off(struct drm_device *dev, int crtc);
 extern void drm_vblank_cleanup(struct drm_device *dev);
+extern struct timeval drm_get_timestamp(void);
 extern u32 drm_get_last_vbltimestamp(struct drm_device *dev, int crtc,
 				     struct timeval *tvblank, unsigned flags);
 extern int drm_calc_vbltimestamp_from_scanoutpos(struct drm_device *dev,
