2dcff3503014508fada3e5dc095708d1294e830f
Stéphane Marchesin <marcheu@chromium.org>
2014-01-27 18:26:22 -0800

    Revert "BACKPORT: drm/i915: update FBC maximum fb sizes"
    This reverts commit dcf4377b17d7b8d2ab4ab708aef9c914047287c9.
    
    It seems like the larger FBC sizes don't work at the hw level; they
    cause GPU lockups and complete machine freezes. This is fairly
    difficult to reproduce, so went unnoticed for a while.
    
    Bug chromium:338588 has been filed to track solving this mystery and
    reenabling FBC on link. For now, we just disable it and ship this
    thing.
    
    BUG=chromium:328929
    TEST=on 8 link machines, run vimeo + webgl aquarium + youtube for 2 hours (so 16 hours of runtime total), no crash
    
    Change-Id: I3098fd620fcb4ea0e4dc88372f7c45a6cf686563
    Reviewed-on: https://chromium-review.googlesource.com/184063
    Tested-by: Stéphane Marchesin <marcheu@chromium.org>
    Reviewed-by: Dominik Behr <dbehr@chromium.org>
    Commit-Queue: Stéphane Marchesin <marcheu@chromium.org>

--- drivers/gpu/drm/i915/intel_pm.c
+++ drivers/gpu/drm/i915/intel_pm.c
@@ -430,7 +430,7 @@ void intel_disable_fbc(struct drm_device *dev)
  *   - no pixel mulitply/line duplication
  *   - no alpha buffer discard
  *   - no dual wide
- *   - framebuffer <= max_hdisplay in width, max_vdisplay in height
+ *   - framebuffer <= 2048 in width, 1536 in height
  *
  * We can't assume that any compression will take place (worst case),
  * so the compressed buffer has to be the same size as the uncompressed
@@ -448,7 +448,6 @@ void intel_update_fbc(struct drm_device *dev)
 	struct intel_framebuffer *intel_fb;
 	struct drm_i915_gem_object *obj;
 	int enable_fbc;
-	unsigned int max_hdisplay, max_vdisplay;
 
 	if (!i915_powersave)
 		return;
@@ -513,16 +512,8 @@ void intel_update_fbc(struct drm_device *dev)
 		dev_priv->no_fbc_reason = FBC_UNSUPPORTED_MODE;
 		goto out_disable;
 	}
-
-	if (IS_G4X(dev) || INTEL_INFO(dev)->gen >= 5) {
-		max_hdisplay = 4096;
-		max_vdisplay = 2048;
-	} else {
-		max_hdisplay = 2048;
-		max_vdisplay = 1536;
-	}
-	if ((crtc->mode.hdisplay > max_hdisplay) ||
-	    (crtc->mode.vdisplay > max_vdisplay)) {
+	if ((crtc->mode.hdisplay > 2048) ||
+	    (crtc->mode.vdisplay > 1536)) {
 		DRM_DEBUG_KMS("mode too large for compression, disabling\n");
 		dev_priv->no_fbc_reason = FBC_MODE_TOO_LARGE;
 		goto out_disable;
