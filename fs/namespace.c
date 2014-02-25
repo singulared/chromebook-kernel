6af229957ec7fbed1841eb4989c7f9e8e9f67692
Eric W. Biederman <ebiederm@xmission.com>
2014-02-24 17:32:34 -0800

    BACKPORT: vfs: factor out lookup_mountpoint from new_mountpoint
    I am shortly going to add a new user of struct mountpoint that
    needs to look up existing entries but does not want to create
    a struct mountpoint if one does not exist.  Therefore to keep
    the code simple and easy to read split out lookup_mountpoint
    from new_mountpoint.
    
    BUG=chromium:773474
    TEST=build/boot on link, run xfsFilesystemTestSuiteExt4Plain
    
    Change-Id: I7a5f9d13cb1d31265682d12549a395dca1f07fa0
    Signed-off-by: "Eric W. Biederman" <ebiederm@xmission.com>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    (cherry picked from commit e2dfa935464272395b4f35f4cc74ffcc87418b84)
    Signed-off-by: Sonny Rao <sonnyrao@chromium.org>
    [SR: we don't have mp_hash() from
       0818bf27c05b2d resizable namespace.c hashes
       so go back to older way of accessing hash]
    Reviewed-on: https://chromium-review.googlesource.com/762634
    Tested-by: Gwendal Grignou <gwendal@chromium.org>
    Reviewed-by: Gwendal Grignou <gwendal@chromium.org>

--- fs/namespace.c
+++ fs/namespace.c
@@ -607,11 +607,10 @@ struct vfsmount *lookup_mnt(struct path *path)
 	}
 }
 
-static struct mountpoint *new_mountpoint(struct dentry *dentry)
+static struct mountpoint *lookup_mountpoint(struct dentry *dentry)
 {
 	struct list_head *chain = mountpoint_hashtable + hash(NULL, dentry);
 	struct mountpoint *mp;
-	int ret;
 
 	list_for_each_entry(mp, chain, m_hash) {
 		if (mp->m_dentry == dentry) {
@@ -622,6 +621,14 @@ static struct mountpoint *new_mountpoint(struct dentry *dentry)
 			return mp;
 		}
 	}
+	return NULL;
+}
+
+static struct mountpoint *new_mountpoint(struct dentry *dentry)
+{
+	struct list_head *chain = mountpoint_hashtable + hash(NULL, dentry);
+	struct mountpoint *mp;
+	int ret;
 
 	mp = kmalloc(sizeof(struct mountpoint), GFP_KERNEL);
 	if (!mp)
@@ -1616,7 +1623,9 @@ retry:
 	down_write(&namespace_sem);
 	mnt = lookup_mnt(path);
 	if (likely(!mnt)) {
-		struct mountpoint *mp = new_mountpoint(dentry);
+		struct mountpoint *mp = lookup_mountpoint(dentry);
+		if (!mp)
+			mp = new_mountpoint(dentry);
 		if (IS_ERR(mp)) {
 			up_write(&namespace_sem);
 			mutex_unlock(&dentry->d_inode->i_mutex);
