6fd7193f05e81a819420aad8ede11e8f09fe1159
Doug Anderson <dianders@chromium.org>
2014-02-27 09:35:00 -0800

    exynos542x: devfreq: Promote to device_initcall
    We should run devfreq a little earlier.  This is important on 5422
    where it's important that the INT voltage is high enough to run the
    CPU clock at maximum speed.  The exynos-cpufreq is a late initcall.
    
    BUG=chrome-os-partner:25818
    TEST=Add prints in init and make sure devfreq happens before cpufreq.
    See I35d132a9d4533a131bd582e3a30360f2e413d51c.
    
    Change-Id: I8decb4aa5b754628db0ff5f12c1b2ed6b0043fe7
    Signed-off-by: Doug Anderson <dianders@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/188170
    Reviewed-by: Andrew Bresticker <abrestic@chromium.org>

--- drivers/devfreq/exynos5420_bus_int.c
+++ drivers/devfreq/exynos5420_bus_int.c
@@ -1082,7 +1082,7 @@ static int __init exynos5_busfreq_int_init(void)
 {
 	return platform_driver_register(&exynos5_busfreq_int_driver);
 }
-late_initcall(exynos5_busfreq_int_init);
+device_initcall(exynos5_busfreq_int_init);
 
 static void __exit exynos5_busfreq_int_exit(void)
 {
