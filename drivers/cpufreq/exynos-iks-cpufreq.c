1e9ef2b13a27cb659b72f3bdb31cf5d1981352b8
Akshu Agrawal <akshu.a@samsung.com>
2014-03-03 19:22:51 +0530

    Revert "cpufreq: exynos-iks: use in-kernel-switcher for 5422"
    This reverts commit 5f3135358eb2804c8fda2bc93c1004145323e5a6.
    
    We have seen considerable performance improvement with IKS_Lite.
    The impact on power is not significant compared to IKS.
    Since, performance and general browsing experience are better with
    IKS_Lite, enable for both pit and pi.
    
    BUG=chrome-os-partner:25976
    TEST= Ran power and performance benchmarks
    
    Change-Id: I2b9e5a7403ba5c00b3d1dc5dc50eb5fb571f966b
    Signed-off-by: Akshu Agrawal <akshu.a@samsung.com>
    Reviewed-on: https://chromium-review.googlesource.com/188533
    Reviewed-by: kimoon kim <kimoon.kim@samsung.com>
    Reviewed-by: Doug Anderson <dianders@chromium.org>
    Reviewed-by: Andrew Bresticker <abrestic@chromium.org>
    Commit-Queue: Abhinav Kochhar <abhinav.k@samsung.com>
    Tested-by: Abhinav Kochhar <abhinav.k@samsung.com>

--- drivers/cpufreq/exynos-iks-cpufreq.c
+++ drivers/cpufreq/exynos-iks-cpufreq.c
@@ -71,7 +71,7 @@ static DEFINE_PER_CPU(unsigned int, req_freq);
 
 static struct cpufreq_policy fake_policy[CA_END][NR_CPUS];
 
-static bool use_cluster_switching;
+static bool use_cluster_switching = true;
 
 static void switch_complete_cb(void *cookie)
 {
@@ -758,9 +758,6 @@ int __init exynos_iks_cpufreq_init(void)
 	int cpu;
 
 	boot_cluster = 0;
-
-	if (soc_is_exynos5420())
-		use_cluster_switching = true;
 
 	if (!bL_switcher_get_enabled()) {
 		bL_switcher_put_enabled();
