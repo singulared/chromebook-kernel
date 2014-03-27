YaroST12/crdroid_kernel_oneplus_sdm845

drm/prime: double lock typo
There is a typo so deadlocks on error instead of unlocking.

Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
Reviewed-by: Daniel Vetter <daniel.vetter@ffwll.ch>
Signed-off-by: Dave Airlie <airlied@gmail.com>

drivers/gpu/drm/drm_prime.c
@@ -552,7 +552,7 @@ fail:
 	 */
 	drm_gem_handle_delete(file_priv, *handle);
 out_unlock:
-	mutex_lock(&dev->object_name_lock);
+	mutex_unlock(&dev->object_name_lock);
 out_put:
 	dma_buf_put(dma_buf);
 	mutex_unlock(&file_priv->prime.lock);

singulared/chromebook-kernel

(cherry picked from commit 0adb23709ba9dd87d8bfa1ee349482ac8ec0730a)
Signed-off-by: Stéphane Marchesin <marcheu@chromium.org>
Conflicts:
	drivers/gpu/drm/drm_prime.c
BUG=chromium:340541
TEST=builds and runs on link and daisy
Change-Id: Iac6b4041bb5259d0a74627a40709830374fb44b9
Reviewed-on: https://chromium-review.googlesource.com/190652
Tested-by: Stéphane Marchesin <marcheu@chromium.org>
Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>
Commit-Queue: Stéphane Marchesin <marcheu@chromium.org>

drivers/gpu/drm/drm_prime.c
@@ -552,7 +552,7 @@ fail:
 	 */
 	drm_gem_handle_delete(file_priv, *handle);
 out_unlock:
-	spin_lock(&dev->object_name_lock);
+	spin_unlock(&dev->object_name_lock);
 out_put:
 	dma_buf_put(dma_buf);
 	mutex_unlock(&file_priv->prime.lock);