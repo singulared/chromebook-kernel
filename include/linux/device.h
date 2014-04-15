65cfe6db3e3b1d3ca97d6ebb70b031cf037c1054
Manish Badarkhe <badarkhe.manish@gmail.com>
2014-04-15 10:28:19 +0530

    UPSTREAM: devres: introduce API "devm_kstrdup"
    This patch introduces "devm_kstrdup" API so that the
    device's driver can allocate memory and copy string.
    
    BUG=chrome-os-partner:26245
    TEST=boot pi
    
    Change-Id: I975ca681e6260f06147aafc7e2220126b119cd3b
    Signed-off-by: Manish Badarkhe <badarkhe.manish@gmail.com>
    Signed-off-by: Mark Brown <broonie@linaro.org>
    (cherry-picked from commit e31108cad3deabb1a63111d7aa699ca67753c01f)
    [Inderpal Resolved merge conflicts, used kzalloc instead of kmalloc]
    Signed-off-by: Inderpal Singh <inderpal.s@samsung.com>
    Reviewed-on: https://chromium-review.googlesource.com/194931
    Reviewed-by: Doug Anderson <dianders@chromium.org>

--- include/linux/device.h
+++ include/linux/device.h
@@ -573,6 +573,7 @@ extern int devres_release_group(struct device *dev, void *id);
 /* managed kzalloc/kfree for device drivers, no kmalloc, always use kzalloc */
 extern void *devm_kzalloc(struct device *dev, size_t size, gfp_t gfp);
 extern void devm_kfree(struct device *dev, void *p);
+extern char *devm_kstrdup(struct device *dev, const char *s, gfp_t gfp);
 
 void __iomem *devm_ioremap_resource(struct device *dev, struct resource *res);
 void __iomem *devm_request_and_ioremap(struct device *dev,
