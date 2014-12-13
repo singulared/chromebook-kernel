ef6ade4ae6ba1db2475ae18de92f334914b9266a
Zach Reizner <zachr@chromium.org>
2014-12-12 16:04:50 -0800

    CHROMIUM: drm/cirrus: implement gem_prime_mmap
    Instead of using the regular ttm_bo_mmap function, which is meant to be
    called from a dumb mmap, this patch uses ttm_fbdev_mmap. This takes
    any ttm_buffer_object we give it and still retains all the useful
    functions that manage the memory map as the underlying buffers gets
    moved around system and video RAM.
    
    TEST=use vgem to mmap a prime shared buffer from cirrus
    BUG=chromium:394868
    CQ-DEPEND=CL:235715
    
    Change-Id: I604b562de7368e28616e9c2214bf4d5d12bc32b4
    Signed-off-by: Zach Reizner <zachr@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/235791
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>

--- drivers/gpu/drm/cirrus/cirrus_drv.h
+++ drivers/gpu/drm/cirrus/cirrus_drv.h
@@ -254,6 +254,8 @@ struct drm_gem_object *cirrus_gem_prime_import_sg_table(struct drm_device *dev,
 							struct sg_table *sg);
 int cirrus_gem_prime_pin(struct drm_gem_object *obj);
 void cirrus_gem_prime_unpin(struct drm_gem_object *obj);
+int cirrus_gem_prime_mmap(struct drm_gem_object *obj,
+			  struct vm_area_struct *vma);
 
 
 #endif				/* __CIRRUS_DRV_H__ */
