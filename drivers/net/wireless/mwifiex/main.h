46a6fdf579b6985baee16233ad942c464a901204
Zhaoyang Liu <liuzy@marvell.com>
2015-03-17 01:28:43 -0700

    UPSTREAM: mwifiex: preprocess packets from TX queue
    During profiling, we discovered that driver remains idle for time
    when pakcet is downloaded to FW but no TX_DONE has been received
    i.e. while data_sent is true.
    
    This patch adds enhancement to TX routine where we preprocess
    packets from TX queue, make them ready for TX and add them to
    separate TX queue.
    
    BUG=chrome-os-partner:33858
    TEST=Ran throughput test(iperf TX/RX) with all patches in this series applied,
         TP improvements seen around 25%. No Google autotest regressions.
    
    Signed-off-by: Zhaoyang Liu <liuzy@marvell.com>
    Signed-off-by: Chin-ran Lo <crlo@marvell.com>
    Reviewed-by: Cathy Luo <cluo@marvell.com>
    Reviewed-by: Amitkumar Karwar <akarwar@marvell.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>
    Signed-off-by: Avinash Patil <patila@marvell.com>
    Change-Id: Id2960a108d5e126e322d21e8148114881a81dc7d
    Reviewed-on: https://chromium-review.googlesource.com/260771
    Reviewed-by: Paul Stewart <pstew@chromium.org>

--- drivers/net/wireless/mwifiex/main.h
+++ drivers/net/wireless/mwifiex/main.h
@@ -49,6 +49,8 @@ enum {
 
 #define MWIFIEX_MAX_AP				64
 
+#define MWIFIEX_MAX_PKTS_TXQ			16
+
 #define MWIFIEX_DEFAULT_WATCHDOG_TIMEOUT	(5 * HZ)
 
 #define MWIFIEX_TIMER_10S			10000
@@ -752,6 +754,8 @@ struct mwifiex_adapter {
 	spinlock_t scan_pending_q_lock;
 	/* spin lock for RX processing routine */
 	spinlock_t rx_proc_lock;
+	struct sk_buff_head tx_data_q;
+	atomic_t tx_queued;
 	struct sk_buff_head usb_rx_data_q;
 	u32 scan_processing;
 	u16 region_code;
@@ -828,6 +832,8 @@ struct mwifiex_adapter {
 	struct semaphore *card_sem;
 	bool scan_chan_gap_enabled;
 };
+
+void mwifiex_process_tx_queue(struct mwifiex_adapter *adapter);
 
 int mwifiex_init_lock_list(struct mwifiex_adapter *adapter);
 
