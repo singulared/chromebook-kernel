6c7cc2055ebd425a29cf269b691e0154cc3c5860
Smitha T Murthy <smitha.t@samsung.com>
2015-03-23 12:03:03 +0530

    CHROMIUM: s5p-mfc: Copy timestamp and timecode in encoder.
    Adding support to copy the timestamp and timecode of the
    source buffers of the mfc encoder into its destination
    buffers.
    
    BUG=chrome-os-partner:30604
    TEST= Tested using mainline application v4l2-mfc-encoder.
    Altered the test application at the v4l_enq_buf call to add
    the timestamp for the source buffer. Added prints to check
    the timestamp of source buffer and destination buffer and
    confirmed with values obtained, copying  was successful.
    
    Change-Id: I0d44d6b7b867ce0adee213591185051ccfd715fc
    Signed-off-by: Smitha T Murthy <smitha.t@samsung.com>
    Reviewed-on: https://chromium-review.googlesource.com/261880
    Reviewed-by: Haixia Shi <hshi@chromium.org>
    Commit-Queue: Haixia Shi <hshi@chromium.org>
    Tested-by: Haixia Shi <hshi@chromium.org>

--- drivers/media/platform/s5p-mfc/s5p_mfc_enc.c
+++ drivers/media/platform/s5p-mfc/s5p_mfc_enc.c
@@ -821,6 +821,7 @@ static int enc_post_frame_start(struct s5p_mfc_ctx *ctx, unsigned int reason,
 {
 	struct s5p_mfc_dev *dev = ctx->dev;
 	struct s5p_mfc_buf *mb_entry;
+	struct s5p_mfc_buf *dst_buf;
 	unsigned long enc_y_addr, enc_c_addr;
 	unsigned long mb_y_addr, mb_c_addr;
 	int slice_type;
@@ -840,8 +841,14 @@ static int enc_post_frame_start(struct s5p_mfc_ctx *ctx, unsigned int reason,
 		list_for_each_entry(mb_entry, &ctx->src_queue, list) {
 			mb_y_addr = vb2_dma_contig_plane_dma_addr(mb_entry->b, 0);
 			mb_c_addr = vb2_dma_contig_plane_dma_addr(mb_entry->b, 1);
+			dst_buf = list_entry(ctx->dst_queue.next,
+					struct s5p_mfc_buf, list);
 			if ((enc_y_addr == mb_y_addr) &&
 						(enc_c_addr == mb_c_addr)) {
+				dst_buf->b->v4l2_buf.timecode =
+					mb_entry->b->v4l2_buf.timecode;
+				dst_buf->b->v4l2_buf.timestamp =
+					mb_entry->b->v4l2_buf.timestamp;
 				list_del(&mb_entry->list);
 				ctx->src_queue_cnt--;
 				vb2_buffer_done(mb_entry->b,
@@ -852,8 +859,14 @@ static int enc_post_frame_start(struct s5p_mfc_ctx *ctx, unsigned int reason,
 		list_for_each_entry(mb_entry, &ctx->ref_queue, list) {
 			mb_y_addr = vb2_dma_contig_plane_dma_addr(mb_entry->b, 0);
 			mb_c_addr = vb2_dma_contig_plane_dma_addr(mb_entry->b, 1);
+			dst_buf = list_entry(ctx->dst_queue.next,
+					struct s5p_mfc_buf, list);
 			if ((enc_y_addr == mb_y_addr) &&
 						(enc_c_addr == mb_c_addr)) {
+				dst_buf->b->v4l2_buf.timecode =
+					mb_entry->b->v4l2_buf.timecode;
+				dst_buf->b->v4l2_buf.timestamp =
+					mb_entry->b->v4l2_buf.timestamp;
 				list_del(&mb_entry->list);
 				ctx->ref_queue_cnt--;
 				vb2_buffer_done(mb_entry->b,
