c6c9f843c9cea787511e78202057d40af1fa58cd
Dominik Behr <dbehr@chromium.org>
2015-04-02 16:31:48 -0700

    CHROMIUM: drm/i915: special detect path for PS8617 DP-VGA adapter
    The adapter in question fails to read EDID with multiple DDC I2C failures.
    Logic analyzer connected to the DDC lines on VGA shows malformed I2C
    transactions. Forcing slower I2C speeds using DP_I2C_SPEED_CONTROL_STATUS
    does not help.
    
    Add a special detection path for this adapter and report monitor present
    if there is 1 sink present. EDID modes will not be availale but at least
    VGA modes will be and Chromebox will display UI.
    
    Also, call intel_dp_probe_oui right after reading DPCD, so Apple VGA
    adapter detection happens before it is used.
    
    BUG=chromium:438072
    TEST=connect Asus VH192 via PS8617 DP-VGA dongle to Panther
    
    Change-Id: Id143b4149ff9693efd2282cf687d6c3bf54115db
    Signed-off-by: Dominik Behr <dbehr@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/263803
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>

--- drivers/gpu/drm/i915/intel_dp.c
+++ drivers/gpu/drm/i915/intel_dp.c
@@ -2025,6 +2025,7 @@ intel_dp_probe_oui(struct intel_dp *intel_dp)
 			}
 		}
 	}
+	intel_dp->is_ps8617 = drm_dp_branch_is_ps8617(buf);
 
 	ironlake_edp_panel_vdd_off(intel_dp, false);
 }
@@ -2110,6 +2111,8 @@ intel_dp_detect_dpcd(struct intel_dp *intel_dp)
 	if (!intel_dp_get_dpcd(intel_dp))
 		return connector_status_disconnected;
 
+	intel_dp_probe_oui(intel_dp);
+
 	/* if there's no downstream port, we're done */
 	if (!(dpcd[DP_DOWNSTREAMPORT_PRESENT] & DP_DWN_STRM_PORT_PRESENT))
 		return connector_status_connected;
@@ -2151,6 +2154,23 @@ intel_dp_detect_dpcd(struct intel_dp *intel_dp)
 	type = intel_dp->downstream_ports[0] & DP_DS_PORT_TYPE_MASK;
 	if (type == DP_DS_PORT_TYPE_VGA || type == DP_DS_PORT_TYPE_NON_EDID)
 		return connector_status_unknown;
+
+	/*
+	 * crbug.com/438072 HSW Chromeboxes + Parade PS8617 based dongle and
+	 * Asus VH192. The dongle repeatedly fails DDC I2C transactions. Also
+	 * does not report HPD. Detect it, report that monitor is connected if
+	 * there is a sink present. Because of I2C errors EDID modes will not
+	 * be present but at least VGA modes will be.
+	 */
+	if (intel_dp->is_ps8617) {
+		uint8_t reg;
+		if (intel_dp_dpcd_read_wake(&intel_dp->aux, DP_SINK_COUNT,
+					    &reg, 1) < 0)
+			return connector_status_unknown;
+
+		return DP_GET_SINK_COUNT(reg) ? connector_status_connected
+					      : connector_status_disconnected;
+	}
 
 	/* Anything else is out of spec, warn and ignore */
 	DRM_DEBUG_KMS("Broken DP branch device, ignoring\n");
@@ -2285,8 +2305,6 @@ intel_dp_detect(struct drm_connector *connector, bool force)
 			intel_dp_do_sink_dpms(intel_dp, connector->dpms);
 		return status;
 	}
-
-	intel_dp_probe_oui(intel_dp);
 
 	if (intel_dp->force_audio != HDMI_AUDIO_AUTO) {
 		intel_dp->has_audio = (intel_dp->force_audio == HDMI_AUDIO_ON);
