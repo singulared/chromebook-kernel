945cf3374ea227e3f1e831a5c8c88764a891d778
Luigi Semenzato <semenzato@chromium.org>
2015-05-01 16:53:50 -0700

    CHROMIUM: tpm: log every TPM command
    This doesn't happen very often and it is useful
    beyond the immediate need (BUG).  I recommend we
    upstream it.
    
    BUG=chrome-os-partner:26289
    TEST=verified that syslog now contains these messages (about 50 at boot)
    BRANCH=none
    Signed-off-by: Luigi Semenzato <semenzato@chromium.org>
    
    Change-Id: Ib81c2671588064ea4754ec17b0536ed3f0bd9341
    Reviewed-on: https://chromium-review.googlesource.com/268899
    Commit-Queue: Luigi Semenzato <semenzato@chromium.org>
    Tested-by: Luigi Semenzato <semenzato@chromium.org>
    Reviewed-by: Sameer Nanda <snanda@chromium.org>

--- drivers/char/tpm/tpm.c
+++ drivers/char/tpm/tpm.c
@@ -583,6 +583,8 @@ out:
 	if (chip->cpend_flag_set_cb && (tag == 0xc2 || tag == 0xc3))
 		tpm_clear_cpend_flag(chip);
 	mutex_unlock(&chip->tpm_mutex);
+	dev_info(chip->dev, "command 0x%x (size %d) returned code 0x%x\n",
+		 ordinal, count, be32_to_cpu(*((__be32 *) (buf + 6))));
 	return rc;
 }
 
