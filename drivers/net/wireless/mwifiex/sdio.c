d3ae4b5156867610d1901ff9eb573899de59ac26
Aniket Nagarnaik <aniketn@marvell.com>
2015-08-06 00:39:16 -0700

    UPSTREAM: mwifiex: claim sdio bus while downloading the firmware
    Our 8887 A2 chip can have separate firmware images for
    bluetooth and WLAN. We observed an issue during parallel
    downloading of these images by btmrvl and mwifiex drivers.
    
    This patch claims sdio bus before starting of the firmware
    download in mwifiex and releases it after completion to
    fix the problem.
    
    Signed-off-by: Aniket Nagarnaik <aniketn@marvell.com>
    Signed-off-by: Amitkumar Karwar <akarwar@marvell.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>
    (cherry picked from commit cb9d61e5304e2880180e4ccf685a298c868094e0)
    Signed-off-by: Wei-Ning Huang <wnhuang@google.com>
    
    BUG=chrome-os-partner:50138
    TEST=`emerge-oak chromeos-kernel-3_18`
    CQ-DEPEND=CL:332556
    
    Change-Id: Ie393d6b7f9ba858eb91acab5b5e7cc3fe9a37aac
    Reviewed-on: https://chromium-review.googlesource.com/332407
    Commit-Ready: Wei-Ning Huang <wnhuang@chromium.org>
    Tested-by: Wei-Ning Huang <wnhuang@chromium.org>
    Reviewed-by: Daniel Kurtz <djkurtz@chromium.org>

--- drivers/net/wireless/mwifiex/sdio.c
+++ drivers/net/wireless/mwifiex/sdio.c
@@ -837,6 +837,8 @@ static int mwifiex_prog_fw_w_helper(struct mwifiex_adapter *adapter,
 	if (!fwbuf)
 		return -ENOMEM;
 
+	sdio_claim_host(card->func);
+
 	/* Perform firmware data transfer */
 	do {
 		/* The host polls for the DN_LD_CARD_RDY and CARD_IO_READY
@@ -941,6 +943,8 @@ static int mwifiex_prog_fw_w_helper(struct mwifiex_adapter *adapter,
 
 		offset += txlen;
 	} while (true);
+
+	sdio_release_host(card->func);
 
 	mwifiex_dbg(adapter, MSG,
 		    "info: FW download over, size %d bytes\n",
