c65ed563cd30894e1c474da17231758bd76cecd1
henryhsu <henryhsu@chromium.org>
2015-10-05 19:02:50 +0800

    CHROMIUM: [media] videobuf2-dc: Support cacheable MMAP
    DMA allocations for MMAP type is uncached as default. But for
    some cases, CPU has to access the buffers. ie: memcpy for format
    converter. Supporting cacheable MMAP improves huge performance.
    
    BUG=chromium:426383
    TEST=manully test on ARM platform with iommu_ops and arm_dma_ops.
    
    Change-Id: I73f21d586efc5d6cc72d20932ef26d5cdb1ba391
    Signed-off-by: Heng-Ruey Hsu <henryhsu@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/303250
    Commit-Ready: Heng-ruey Hsu <henryhsu@chromium.org>
    Tested-by: Heng-ruey Hsu <henryhsu@chromium.org>
    Reviewed-by: Tomasz Figa <tfiga@chromium.org>

--- drivers/media/v4l2-core/videobuf2-dma-contig.c
+++ drivers/media/v4l2-core/videobuf2-dma-contig.c
@@ -150,10 +150,36 @@ static void vb2_dc_put(void *buf_priv)
 		sg_free_table(buf->sgt_base);
 		kfree(buf->sgt_base);
 	}
+	if (buf->dma_sgt) {
+		sg_free_table(buf->dma_sgt);
+		kfree(buf->dma_sgt);
+	}
 	dma_free_attrs(buf->dev, buf->size, buf->cookie, buf->dma_addr,
 			&buf->attrs);
 	put_device(buf->dev);
 	kfree(buf);
+}
+
+static struct sg_table *vb2_dc_get_base_sgt(struct vb2_dc_buf *buf)
+{
+	int ret;
+	struct sg_table *sgt;
+
+	sgt = kmalloc(sizeof(*sgt), GFP_KERNEL);
+	if (!sgt) {
+		dev_err(buf->dev, "failed to alloc sg table\n");
+		return NULL;
+	}
+
+	ret = dma_get_sgtable_attrs(buf->dev, sgt, buf->cookie, buf->dma_addr,
+		buf->size, &buf->attrs);
+	if (ret < 0) {
+		dev_err(buf->dev, "failed to get scatterlist from DMA API\n");
+		kfree(sgt);
+		return NULL;
+	}
+
+	return sgt;
 }
 
 static void *vb2_dc_alloc(void *alloc_ctx, unsigned long size,
@@ -187,6 +213,10 @@ static void *vb2_dc_alloc(void *alloc_ctx, unsigned long size,
 	buf->handler.refcount = &buf->refcount;
 	buf->handler.put = vb2_dc_put;
 	buf->handler.arg = buf;
+
+	if (!dma_get_attr(DMA_ATTR_NO_KERNEL_MAPPING, &buf->attrs) &&
+	    dma_get_attr(DMA_ATTR_NON_CONSISTENT, &buf->attrs))
+		buf->dma_sgt = vb2_dc_get_base_sgt(buf);
 
 	atomic_inc(&buf->refcount);
 
@@ -222,6 +252,11 @@ static int vb2_dc_mmap(void *buf_priv, struct vm_area_struct *vma)
 	vma->vm_ops		= &vb2_common_vm_ops;
 
 	vma->vm_ops->open(vma);
+
+	if (dma_get_attr(DMA_ATTR_NO_KERNEL_MAPPING, &buf->attrs) &&
+	    dma_get_attr(DMA_ATTR_NON_CONSISTENT, &buf->attrs) &&
+	    !buf->dma_sgt)
+		buf->dma_sgt = vb2_dc_get_base_sgt(buf);
 
 	pr_debug("%s: mapped dma addr 0x%08lx at 0x%08lx, size %ld\n",
 		__func__, (unsigned long)buf->dma_addr, vma->vm_start,
@@ -380,28 +415,6 @@ static struct dma_buf_ops vb2_dc_dmabuf_ops = {
 	.mmap = vb2_dc_dmabuf_ops_mmap,
 	.release = vb2_dc_dmabuf_ops_release,
 };
-
-static struct sg_table *vb2_dc_get_base_sgt(struct vb2_dc_buf *buf)
-{
-	int ret;
-	struct sg_table *sgt;
-
-	sgt = kmalloc(sizeof(*sgt), GFP_KERNEL);
-	if (!sgt) {
-		dev_err(buf->dev, "failed to alloc sg table\n");
-		return NULL;
-	}
-
-	ret = dma_get_sgtable_attrs(buf->dev, sgt, buf->cookie, buf->dma_addr,
-		buf->size, &buf->attrs);
-	if (ret < 0) {
-		dev_err(buf->dev, "failed to get scatterlist from DMA API\n");
-		kfree(sgt);
-		return NULL;
-	}
-
-	return sgt;
-}
 
 static struct dma_buf *vb2_dc_get_dmabuf(void *buf_priv, unsigned long flags)
 {
