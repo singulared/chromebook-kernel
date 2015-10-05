70a81fcdbf8a01bfaef5da52a5c2b92a27842d45
henryhsu <henryhsu@chromium.org>
2015-10-05 18:55:00 +0800

    CHROMIUM: ARM: dma-mapping: Handle prot with non-consistent dma attribute
    If incoming prot is cached, return prot directly with
    DMA_ATTR_NON_CONSISTENT attribute instead of overriding it.
    
    BUG=chromium:426383
    TEST=manully test on ARM platform
    Signed-off-by: Heng-Ruey Hsu <henryhsu@chromium.org>
    
    Change-Id: I24f8fcd4bbfc444ad0ea7487ea305784a418ba7d
    Reviewed-on: https://chromium-review.googlesource.com/304031
    Commit-Ready: Heng-ruey Hsu <henryhsu@chromium.org>
    Tested-by: Heng-ruey Hsu <henryhsu@chromium.org>
    Reviewed-by: Tomasz Figa <tfiga@chromium.org>

--- arch/arm/mm/dma-mapping.c
+++ arch/arm/mm/dma-mapping.c
@@ -568,6 +568,9 @@ static void __free_from_contiguous(struct device *dev, struct page *page,
 
 static inline pgprot_t __get_dma_pgprot(struct dma_attrs *attrs, pgprot_t prot)
 {
+	if (dma_get_attr(DMA_ATTR_NON_CONSISTENT, attrs))
+		return prot;
+
 	prot = dma_get_attr(DMA_ATTR_WRITE_COMBINE, attrs) ?
 			    pgprot_writecombine(prot) :
 			    pgprot_dmacoherent(prot);
