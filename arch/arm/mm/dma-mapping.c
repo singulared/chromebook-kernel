dabc189d165f9d89073a27041ed4c898667e3183
henryhsu <henryhsu@chromium.org>
2015-10-15 11:47:03 +0800

    CHROMIUM: ARM: dma-mapping: Support cache sync after arm_dma_get_sgtable
    Currently arm_dma_sync_sg requires dma_address to do cache sync.
    But dma address is set by map_sg(). We can't do cache sync after
    arm_dma_get_sgtable. The behavior is different from iommu_ops.
    
    We don't have to get dma address in arm_dma_sync_sg_* and convert
    the address to physical address in arm_dma_sync_single_*.
    Use sg_page to get physical address and do sync directly.
    
    BUG=chromium:426383
    TEST=manually test on ARM platform with arm_dma_ops and iommu_ops.
    Signed-off-by: Heng-Ruey Hsu <henryhsu@chromium.org>
    
    Change-Id: Id42db3ed3127880747f5ff65dc9fe323931fb935
    Reviewed-on: https://chromium-review.googlesource.com/305930
    Commit-Ready: Heng-ruey Hsu <henryhsu@chromium.org>
    Tested-by: Heng-ruey Hsu <henryhsu@chromium.org>
    Reviewed-by: Tomasz Figa <tfiga@chromium.org>

--- arch/arm/mm/dma-mapping.c
+++ arch/arm/mm/dma-mapping.c
@@ -932,13 +932,11 @@ void arm_dma_unmap_sg(struct device *dev, struct scatterlist *sg, int nents,
 void arm_dma_sync_sg_for_cpu(struct device *dev, struct scatterlist *sg,
 			int nents, enum dma_data_direction dir)
 {
-	struct dma_map_ops *ops = get_dma_ops(dev);
 	struct scatterlist *s;
 	int i;
 
 	for_each_sg(sg, s, nents, i)
-		ops->sync_single_for_cpu(dev, sg_dma_address(s), s->length,
-					 dir);
+		__dma_page_dev_to_cpu(sg_page(s), s->offset, s->length, dir);
 }
 
 /**
@@ -951,13 +949,11 @@ void arm_dma_sync_sg_for_cpu(struct device *dev, struct scatterlist *sg,
 void arm_dma_sync_sg_for_device(struct device *dev, struct scatterlist *sg,
 			int nents, enum dma_data_direction dir)
 {
-	struct dma_map_ops *ops = get_dma_ops(dev);
 	struct scatterlist *s;
 	int i;
 
 	for_each_sg(sg, s, nents, i)
-		ops->sync_single_for_device(dev, sg_dma_address(s), s->length,
-					    dir);
+		__dma_page_cpu_to_dev(sg_page(s), s->offset, s->length, dir);
 }
 
 /*
