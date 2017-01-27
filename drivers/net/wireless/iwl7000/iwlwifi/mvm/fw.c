1891070ad61ac5fd482f4edbbac27d2ecfc5e1d9
Luca Coelho <luciano.coelho@intel.com>
2017-01-27 11:05:36 +0200

    CHROMIUM: iwl7000: mvm: move number of words calculation in MFUART to a variable
    The type of the value in the offset calculation was different for
    32-bit and 64-bit architectures, so the format string would cause a
    warning in 32-bit archs.  To avoid this, and to make the code look a
    bit cleaner, move the number of words calculation to an integer
    variable and use that in the loop.
    
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>
    iwl7000-tree: e6cd611484ed2556f458a44d34d580dab071ff13

--- drivers/net/wireless/iwl7000/iwlwifi/mvm/fw.c
+++ drivers/net/wireless/iwl7000/iwlwifi/mvm/fw.c
@@ -301,19 +301,18 @@ void iwl_mvm_mfu_assert_dump_notif(struct iwl_mvm *mvm,
 	struct iwl_rx_packet *pkt = rxb_addr(rxb);
 	struct iwl_mfu_assert_dump_notif *mfu_dump_notif = (void *)pkt->data;
 	__le32 *dump_data = mfu_dump_notif->data;
+	int n_words = le32_to_cpu(mfu_dump_notif->data_size) / sizeof(__le32);
 	int i;
 
 	if (mfu_dump_notif->index_num == 0)
 		IWL_INFO(mvm, "MFUART assert id 0x%x occurred\n",
 			 le32_to_cpu(mfu_dump_notif->assert_id));
 
-	for (i = 0; i < le32_to_cpu(mfu_dump_notif->data_size) /
-			sizeof(__le32); i++)
+	for (i = 0; i < n_words; i++)
 		IWL_DEBUG_INFO(mvm,
-			       "MFUART assert dump, dword %lu: 0x%08x\n",
-			       (le16_to_cpu(mfu_dump_notif->index_num) *
-				le32_to_cpu(mfu_dump_notif->data_size) /
-				sizeof(__le32)) + i,
+			       "MFUART assert dump, dword %u: 0x%08x\n",
+			       le16_to_cpu(mfu_dump_notif->index_num) *
+			       n_words + i,
 			       le32_to_cpu(dump_data[i]));
 }
 
