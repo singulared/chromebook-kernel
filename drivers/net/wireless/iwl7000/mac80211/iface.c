949761aeb7e8672bc023ecc6cd411e6e07a735ad
backports git tracker <>
2017-02-13 09:22:06 +0200

    CHROMIUM: iwl7000: Update backport tree
    x-git-tracker-backport: 8ed9f24cb1b4ab6b65e5424740f5a2ecd5259f22
    x-git-tracker-iwlwifi: 36a2d48fb46fbf43e58559fcf5f339d45847c439
    iwl7000-tree: cd642f2f84cab96b6710ad5028216ecc65c9424d

--- drivers/net/wireless/iwl7000/mac80211/iface.c
+++ drivers/net/wireless/iwl7000/mac80211/iface.c
@@ -1130,20 +1130,17 @@ static void ieee80211_uninit(struct net_device *dev)
 	ieee80211_teardown_sdata(IEEE80211_DEV_TO_SUB_IF(dev));
 }
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(3,14,0) || \
-    (LINUX_VERSION_CODE == KERNEL_VERSION(3,13,11) && UTS_UBUNTU_RELEASE_ABI > 30)
 static u16 ieee80211_netdev_select_queue(struct net_device *dev,
-					 struct sk_buff *skb,
-					 void *accel_priv,
-					 select_queue_fallback_t fallback)
-#elif LINUX_VERSION_CODE >= KERNEL_VERSION(3,13,0)
-static u16 ieee80211_netdev_select_queue(struct net_device *dev,
-					 struct sk_buff *skb,
-					 void *accel_priv)
-#else
-static u16 ieee80211_netdev_select_queue(struct net_device *dev,
-					 struct sk_buff *skb)
+					 struct sk_buff *skb
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(3,13,0)
+,
+					 void *accel_priv
 #endif
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(3,14,0) || (LINUX_VERSION_CODE == KERNEL_VERSION(3,13,11) && UTS_UBUNTU_RELEASE_ABI > 30)
+,
+					 select_queue_fallback_t fallback
+#endif
+)
 {
 	return ieee80211_select_queue(IEEE80211_DEV_TO_SUB_IF(dev), skb);
 }
@@ -1200,20 +1197,17 @@ static const struct net_device_ops ieee80211_dataif_ops = {
 	.ndo_get_stats64	= ieee80211_get_stats64,
 };
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(3,14,0) || \
-    (LINUX_VERSION_CODE == KERNEL_VERSION(3,13,11) && UTS_UBUNTU_RELEASE_ABI > 30)
 static u16 ieee80211_monitor_select_queue(struct net_device *dev,
-					  struct sk_buff *skb,
-					  void *accel_priv,
-					  select_queue_fallback_t fallback)
-#elif LINUX_VERSION_CODE >= KERNEL_VERSION(3,13,0)
-static u16 ieee80211_monitor_select_queue(struct net_device *dev,
-					  struct sk_buff *skb,
-					  void *accel_priv)
-#else
-static u16 ieee80211_monitor_select_queue(struct net_device *dev,
-					  struct sk_buff *skb)
+					  struct sk_buff *skb
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(3,13,0)
+,
+					  void *accel_priv
 #endif
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(3,14,0) || (LINUX_VERSION_CODE == KERNEL_VERSION(3,13,11) && UTS_UBUNTU_RELEASE_ABI > 30)
+,
+					  select_queue_fallback_t fallback
+#endif
+)
 {
 	struct ieee80211_sub_if_data *sdata = IEEE80211_DEV_TO_SUB_IF(dev);
 	struct ieee80211_local *local = sdata->local;
