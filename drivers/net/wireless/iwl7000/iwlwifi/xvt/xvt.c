01370dfa0d5ae05a4bdd8c56dff062dc5bb59546
Nitzan <nitzanx.kugelman@intel.com>
2017-03-02 16:03:43 +0200

    CHROMIUM: iwl7000: xvt: support extended config cmd
    Add support for INIT_EXTENDED_CFG_CMD in xVT init flow.
    Extended config cmd is meant to notify FW which flows to expect
    before init flow is completed. NVM access and debug config are
    always done in the xVT init flow, therefore those flags are
    always set. Phy config is expected to be sent always, except
    in NVM commit flow, in which reset device is done before it, and
    is recognized by INIT load mask.
    
    Signed-off-by: Nitzan <nitzanx.kugelman@intel.com>
    iwl7000-tree: fccc3be434a0caf8f4c225c686e819a1f332f506

--- drivers/net/wireless/iwl7000/iwlwifi/xvt/xvt.c
+++ drivers/net/wireless/iwl7000/iwlwifi/xvt/xvt.c
@@ -153,9 +153,17 @@ static const struct iwl_hcmd_names iwl_xvt_tof_names[] = {
 	HCMD_NAME(LOCATION_RANGE_RESPONSE_NOTIFICATION),
 };
 
+/* Please keep this array *SORTED* by hex value.
+ * Access is done through binary search.
+ */
+static const struct iwl_hcmd_names iwl_xvt_system_names[] = {
+	HCMD_NAME(INIT_EXTENDED_CFG_CMD),
+};
+
 static const struct iwl_hcmd_arr iwl_xvt_cmd_groups[] = {
 	[LEGACY_GROUP] = HCMD_ARR(iwl_xvt_cmd_names),
 	[LONG_GROUP] = HCMD_ARR(iwl_xvt_cmd_names),
+	[SYSTEM_GROUP] = HCMD_ARR(iwl_xvt_system_names),
 	[PHY_OPS_GROUP] = HCMD_ARR(iwl_xvt_phy_names),
 	[DATA_PATH_GROUP] = HCMD_ARR(iwl_xvt_data_path_names),
 	[CMD_GROUP_LOCATION] = HCMD_ARR(iwl_xvt_tof_names),
