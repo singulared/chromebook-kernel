971cef7b60119d8bd1a47e99e8dbc7d4a77b7737
Emmanuel Grumbach <emmanuel.grumbach@intel.com>
2017-03-29 10:21:09 +0300

    CHROMIUM: iwl7000: mvm: fix firmware debug restart recording
    When we want to stop the recording of the firmware debug
    and restart it later without reloading the firmware we
    don't need to resend the configuration that comes with
    host commands.
    Sending those commands confused the hardware and led to
    an NMI 0x66.
    
    Change the flow as following:
    * read the relevant registers (DBGC_IN_SAMPLE, DBGC_OUT_CTRL)
    * clear those registers
    * wait for the hardware to complete its write to the buffer
    * get the data
    * restore the value of those registers (to restart the
      recording)
    
    For early start (where the configuration is already
    compiled in the firmware), we don't need to set those
    registers after the firmware has been loaded, but only
    when we want to restart the recording without having
    restarted the firmware.
    
    Signed-off-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    iwl7000-tree: 5116d0827b948713bc0211e7758b64c697b1786c

--- drivers/net/wireless/iwl7000/iwlwifi/mvm/ops.c
+++ drivers/net/wireless/iwl7000/iwlwifi/mvm/ops.c
@@ -1351,6 +1351,7 @@ static void iwl_mvm_fw_error_dump_wk(struct work_struct *work)
 {
 	struct iwl_mvm *mvm =
 		container_of(work, struct iwl_mvm, fw_dump_wk.work);
+	u32 in_sample, out_ctrl;
 
 	if (iwl_mvm_ref_sync(mvm, IWL_MVM_REF_FW_DBG_COLLECT))
 		return;
@@ -1361,17 +1362,29 @@ static void iwl_mvm_fw_error_dump_wk(struct work_struct *work)
 	if (mvm->cfg->device_family == IWL_DEVICE_FAMILY_7000) {
 		iwl_set_bits_prph(mvm->trans, MON_BUFF_SAMPLE_CTL, 0x100);
 	} else {
+		in_sample = iwl_read_prph(mvm->trans, DBGC_IN_SAMPLE);
+		out_ctrl = iwl_read_prph(mvm->trans, DBGC_OUT_CTRL);
+
 		iwl_write_prph(mvm->trans, DBGC_IN_SAMPLE, 0);
-		/* wait before we collect the data till the DBGC stop */
 		udelay(100);
+		iwl_write_prph(mvm->trans, DBGC_OUT_CTRL, 0);
+		/* wait before we collect the data till the DBGC stop */
+		udelay(500);
 	}
 
 	iwl_mvm_fw_error_dump(mvm);
 
 	/* start recording again if the firmware is not crashed */
-	WARN_ON_ONCE((!test_bit(STATUS_FW_ERROR, &mvm->trans->status)) &&
-		     mvm->fw->dbg_dest_tlv &&
-		     iwl_mvm_start_fw_dbg_conf(mvm, mvm->fw_dbg_conf));
+	if (!test_bit(STATUS_FW_ERROR, &mvm->trans->status) &&
+	    mvm->fw->dbg_dest_tlv) {
+		if (mvm->cfg->device_family == IWL_DEVICE_FAMILY_7000) {
+			iwl_clear_bits_prph(mvm->trans,
+					    MON_BUFF_SAMPLE_CTL, 0x100);
+		} else {
+			iwl_write_prph(mvm->trans, DBGC_IN_SAMPLE, in_sample);
+			iwl_write_prph(mvm->trans, DBGC_OUT_CTRL, out_ctrl);
+		}
+	}
 
 	mutex_unlock(&mvm->mutex);
 
