ee456acd4519d564b1a3139f51e1f9bfde471533
Luigi Semenzato <semenzato@chromium.org>
2017-04-06 14:32:27 -0700

    CHROMIUM: add vmstat counts for different page fault types
    VM event counters are added to keep track of anonymous
    vs. file vs. shmem page faults.  They are: pgmajfault_a,
    pgmajfault_f and pgmajfault_s.  These are useful to
    analyze system performance, particularly when the cost
    of a fault for a file page is very different from that
    of an anonymous page, as would happen, for instance, in
    the presence of zram.
    
    The PGMAJFAULT counter is no longer directly maintained,
    for performance.  Instead the three new counters are added
    whenever the total count is needed.
    
    This adds entries to /proc/vmstat.  I considered adding them at
    the end for better compatibility, but the contents of /proc/vmstat
    are already highly variable depending on config options.  It is
    still worth asking whether the new counters should NOT contain
    "pgmajfault" as a substring of their name.
    
    BUG=chromium:718270
    TEST=checked that new fields are printed
    Signed-off-by: Luigi Semenzato <semenzato@chromium.org>
    
    Change-Id: If51919e4b1197e1e1027e40c538649f611823def
    Reviewed-on: https://chromium-review.googlesource.com/513619
    Commit-Ready: Luigi Semenzato <semenzato@chromium.org>
    Tested-by: Luigi Semenzato <semenzato@chromium.org>
    Reviewed-by: Luigi Semenzato <semenzato@chromium.org>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -110,6 +110,9 @@ enum mem_cgroup_events_index {
 	MEM_CGROUP_EVENTS_PGPGOUT,	/* # of pages paged out */
 	MEM_CGROUP_EVENTS_PGFAULT,	/* # of page-faults */
 	MEM_CGROUP_EVENTS_PGMAJFAULT,	/* # of major page-faults */
+	MEM_CGROUP_EVENTS_PGMAJFAULT_S,	/* # of major shmem page-faults */
+	MEM_CGROUP_EVENTS_PGMAJFAULT_A,	/* # of major anonymous page-faults */
+	MEM_CGROUP_EVENTS_PGMAJFAULT_F,	/* # of major file page-faults */
 	MEM_CGROUP_EVENTS_NSTATS,
 };
 
@@ -118,6 +121,9 @@ static const char * const mem_cgroup_events_names[] = {
 	"pgpgout",
 	"pgfault",
 	"pgmajfault",
+	"pgmajfault_s",
+	"pgmajfault_a",
+	"pgmajfault_f",
 };
 
 /*
@@ -1165,8 +1171,20 @@ void __mem_cgroup_count_vm_event(struct mm_struct *mm, enum vm_event_item idx)
 	case PGFAULT:
 		this_cpu_inc(memcg->stat->events[MEM_CGROUP_EVENTS_PGFAULT]);
 		break;
-	case PGMAJFAULT:
+	case PGMAJFAULT_S:
 		this_cpu_inc(memcg->stat->events[MEM_CGROUP_EVENTS_PGMAJFAULT]);
+		this_cpu_inc(memcg->stat->events[
+				     MEM_CGROUP_EVENTS_PGMAJFAULT_S]);
+		break;
+	case PGMAJFAULT_A:
+		this_cpu_inc(memcg->stat->events[MEM_CGROUP_EVENTS_PGMAJFAULT]);
+		this_cpu_inc(memcg->stat->events[
+				     MEM_CGROUP_EVENTS_PGMAJFAULT_A]);
+		break;
+	case PGMAJFAULT_F:
+		this_cpu_inc(memcg->stat->events[MEM_CGROUP_EVENTS_PGMAJFAULT]);
+		this_cpu_inc(memcg->stat->events[
+				     MEM_CGROUP_EVENTS_PGMAJFAULT_F]);
 		break;
 	default:
 		BUG();
