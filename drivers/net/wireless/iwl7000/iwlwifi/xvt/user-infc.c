027f805d93a10a217d96e8e58f785b8271739d33
Johannes Berg <johannes.berg@intel.com>
2017-04-19 10:04:01 +0200

    CHROMIUM: iwl7000: xvt: fix variable type to catch errors
    Storing -EINVAL returned as an int in a u8 will not result
    in the u8 having a negative value, so the check is broken.
    Use a signed integer and return the error code itself.
    
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>
    iwl7000-tree: 6b7030f1450611aff2d5fd8cf56845d2f5e8f42f

--- drivers/net/wireless/iwl7000/iwlwifi/xvt/user-infc.c
+++ drivers/net/wireless/iwl7000/iwlwifi/xvt/user-infc.c
@@ -1180,10 +1180,10 @@ static int iwl_xvt_tx_queue_cfg(struct iwl_xvt *xvt,
 	struct iwl_xvt_tx_queue_cfg *input =
 			(struct iwl_xvt_tx_queue_cfg *)data_in->data;
 	u8 sta_id = input->sta_id;
-	u8 lmac_id = map_sta_to_lmac(xvt, sta_id);
+	int lmac_id = map_sta_to_lmac(xvt, sta_id);
 
 	if (lmac_id < 0)
-		return -EINVAL;
+		return lmac_id;
 
 	switch (input->operation) {
 	case TX_QUEUE_CFG_ADD:
@@ -1202,7 +1202,8 @@ static int iwl_xvt_modulated_tx_gen2(struct iwl_xvt *xvt,
 	static struct iwl_xvt_tx_mod_task_data task_data;
 	u32 size = sizeof(struct iwl_tm_mod_tx_request);
 	struct tx_meta_data *xvt_tx;
-	u8 sta_id, lmac_id;
+	u8 sta_id;
+	int lmac_id;
 
 	/*
 	* no need to check whether tx already operating on lmac, since check
@@ -1214,7 +1215,7 @@ static int iwl_xvt_modulated_tx_gen2(struct iwl_xvt *xvt,
 	sta_id = task_data.tx_req.sta_id;
 	lmac_id = map_sta_to_lmac(xvt, sta_id);
 	if (lmac_id < 0)
-		return -EINVAL;
+		return lmac_id;
 
 	task_data.lmac_id = lmac_id;
 	xvt_tx = &xvt->tx_meta_data[lmac_id];
