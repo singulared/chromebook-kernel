1dff479ff6af54ef166861534ba67752d9721173
Ben Cheng <bccheng@google.com>
2017-06-08 02:43:42 +0800

    CHROMIUM: low_mem: fix file_size underflow
    Currently we use the following equation to treat some file backed
    memory as free meory:
    
    available_file_mem = file_mem - dirty_mem - min_file_mem;
    
    But sometimes file_mem may be less than min_file_mem, which is a
    constant.
    
    BUG=chromium:730702
    TEST=printed additional debugging info in local testing
    
    Change-Id: Ie5d7015a2609419a1fae4d15647dea229cd4a045
    Reviewed-on: https://chromium-review.googlesource.com/527712
    Commit-Ready: Ben Cheng <bccheng@chromium.org>
    Tested-by: Ben Cheng <bccheng@chromium.org>
    Reviewed-by: Luigi Semenzato <semenzato@chromium.org>
    Signed-off-by: Sonny Rao <sonnyrao@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/543306
    Reviewed-by: Ben Cheng <bccheng@chromium.org>

--- include/linux/low-mem-notify.h
+++ include/linux/low-mem-notify.h
@@ -15,23 +15,41 @@ extern const unsigned long low_mem_anon_mem_delta;
 extern unsigned int low_mem_ram_vs_swap_weight;
 
 /*
- * Compute "available" memory, that is either free memory or memory that can be
- * reclaimed quickly, adjusted for the presence of swap.
+ * Compute available memory used by files that can be reclaimed quickly.
  */
-static inline unsigned long get_available_mem_adj(int lru_base)
+static inline unsigned long get_available_file_mem(int lru_base)
 {
-	/* free_mem is completely unallocated; clean file-backed memory
-	 * (file_mem - dirty_mem) is easy to reclaim, except for the last
-	 * min_filelist_kbytes.
-	 */
-	unsigned long free_mem = global_page_state(NR_FREE_PAGES);
 	unsigned long file_mem =
 			global_page_state(lru_base + LRU_ACTIVE_FILE) +
 			global_page_state(lru_base + LRU_INACTIVE_FILE);
 	unsigned long dirty_mem = global_page_state(NR_FILE_DIRTY);
 	unsigned long min_file_mem = min_filelist_kbytes >> (PAGE_SHIFT - 10);
-	unsigned long available_file_mem = file_mem - dirty_mem - min_file_mem;
-	unsigned long available_mem = free_mem + available_file_mem;
+	unsigned long clean_file_mem = file_mem - dirty_mem;
+	/* Conservatively estimate the amount of available_file_mem */
+	unsigned long available_file_mem = (clean_file_mem > min_file_mem) ?
+	    (clean_file_mem - min_file_mem) : 0;
+	return available_file_mem;
+}
+
+/*
+ * Compute "available" memory, that is either free memory or memory that can be
+ * reclaimed quickly, adjusted for the presence of swap.
+ */
+static inline unsigned long get_available_mem_adj(int lru_base)
+{
+	/* min_free_kbytes is reserved for emergency allocation like when
+	 * PF_MEMALLOC is set. In general it's not usable in normal page
+	 * allocation process.
+	 */
+	unsigned long min_free_pages = min_free_kbytes >> (PAGE_SHIFT - 10);
+	/* free_mem is completely unallocated; clean file-backed memory
+	 * (file_mem - dirty_mem) is easy to reclaim, except for the last
+	 * min_filelist_kbytes.
+	 */
+	unsigned long free_mem =
+			global_page_state(NR_FREE_PAGES) - min_free_pages;
+	unsigned long available_mem = free_mem +
+	    get_available_file_mem(lru_base);
 	/*
 	 * The contribution of swap is reduced by a factor of
 	 * low_mem_ram_vs_swap_weight.
@@ -61,10 +79,13 @@ static inline bool _is_low_mem_situation(void)
 			global_page_state(lru_base + LRU_INACTIVE_ANON);
 		if (unlikely(anon_mem < low_mem_lowest_seen_anon_mem)) {
 			printk(KERN_INFO "entering low_mem "
-			       "(avail RAM = %lu kB, avail swap %lu kB) "
+			       "(avail RAM = %lu kB, avail swap %lu kB, "
+			       "avail file %lu kB) "
 			       "with lowest seen anon mem: %lu kB\n",
 			       available_mem * PAGE_SIZE / 1024,
 			       nr_swap_pages * PAGE_SIZE / 1024,
+			       get_available_file_mem(lru_base) * PAGE_SIZE /
+				  1024,
 			       anon_mem * PAGE_SIZE / 1024);
 			low_mem_lowest_seen_anon_mem = anon_mem -
 				low_mem_anon_mem_delta;
