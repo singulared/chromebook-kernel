ad26e6703accc41a5e1ee93b0f27dac2dbcc9618
Zamir, Roee <roee.zamir@intel.com>
2017-06-08 10:34:16 +0300

    CHROMIUM: iwl7000: mvm: fix the misuse of IWL_MVM_ADWELL_ENABLE
    IWL_MVM_ADWELL_ENABLE shouldn't affect the used API version,
    If it not set, simply don't set IWL_UMAC_SCAN_GEN_FLAGS_ADAPTIVE_DWELL.
    
    Signed-off-by: Zamir, Roee <roee.zamir@intel.com>
    iwl7000-tree: 806f0ca2a272abc4daab0c90192b1626661421b3

--- drivers/net/wireless/iwl7000/iwlwifi/mvm/scan.c
+++ drivers/net/wireless/iwl7000/iwlwifi/mvm/scan.c
@@ -1330,7 +1330,7 @@ static u16 iwl_mvm_scan_umac_flags(struct iwl_mvm *mvm,
 	if (mvm->sched_scan_pass_all == SCHED_SCAN_PASS_ALL_ENABLED)
 		flags |= IWL_UMAC_SCAN_GEN_FLAGS_ITER_COMPLETE;
 
-	if (iwl_mvm_is_adaptive_dwell_supported(mvm) &&
+	if (iwl_mvm_is_adaptive_dwell_supported(mvm) && IWL_MVM_ADWELL_ENABLE &&
 	    vif->type != NL80211_IFTYPE_P2P_DEVICE)
 		flags |= IWL_UMAC_SCAN_GEN_FLAGS_ADAPTIVE_DWELL;
 
