40d48e1cf7aab1c17010f13cdaa8420bbf65013f
Matt Chen <matt.chen@intel.com>
2017-06-23 17:50:18 +0800

    CHROMIUM: iwl7000: mvm: don't send CTDP commands via debugfs if not supported
    Fix this issue if it is not supported by the firmware.
    
    Signed-off-by: Matt Chen <matt.chen@intel.com>
    iwl7000-tree: c03c2b5239e4e55e8329cc269b4618033a111b5f

--- drivers/net/wireless/iwl7000/iwlwifi/mvm/debugfs.c
+++ drivers/net/wireless/iwl7000/iwlwifi/mvm/debugfs.c
@@ -157,6 +157,9 @@ static ssize_t iwl_dbgfs_ctdp_budget_read(struct file *file,
 	char buf[16];
 	int pos, budget;
 
+	if (!iwl_mvm_is_ctdp_supported(mvm))
+		return -EOPNOTSUPP;
+
 	if (!iwl_mvm_firmware_running(mvm) ||
 	    mvm->cur_ucode != IWL_UCODE_REGULAR)
 		return -EIO;
@@ -177,6 +180,9 @@ static ssize_t iwl_dbgfs_stop_ctdp_write(struct iwl_mvm *mvm, char *buf,
 					 size_t count, loff_t *ppos)
 {
 	int ret;
+
+	if (!iwl_mvm_is_ctdp_supported(mvm))
+		return -EOPNOTSUPP;
 
 	if (!iwl_mvm_firmware_running(mvm) ||
 	    mvm->cur_ucode != IWL_UCODE_REGULAR)
