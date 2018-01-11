31ff07ee83c29a4beca6437b44671b1980c94268
Masahiro Yamada <yamada.masahiro@socionext.com>
2018-01-11 22:39:40 +0900

    UPSTREAM: kconfig: make xfgets() really static
    Sparse reports:
      warning: symbol 'xfgets' was not declared. Should it be static?
    
    It is declared as static, but it is missing in the definition part.
    Move the definition up and remove the forward declaration.
    
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>
    (cherry picked from commit 5a3dc717b3c785242e6d40f08288947cb6751ee4)
    
    BUG=chromium:947697
    TEST=build; cd to portage cache; cp cros-old-config cros-old-defconfig;
         build
    
    Change-Id: I915a4ddf770eafa9de718b67b0af35339de00c4f
    Signed-off-by: Douglas Anderson <dianders@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/1547143
    Reviewed-by: Sean Paul <seanpaul@chromium.org>

--- scripts/kconfig/conf.c
+++ scripts/kconfig/conf.c
@@ -18,7 +18,6 @@
 
 static void conf(struct menu *menu);
 static void check_conf(struct menu *menu);
-static void xfgets(char *str, int size, FILE *in);
 
 enum input_mode {
 	oldaskconfig,
@@ -77,6 +76,13 @@ static void check_stdin(void)
 		printf(_("Run 'make oldconfig' to update configuration.\n\n"));
 		exit(1);
 	}
+}
+
+/* Helper function to facilitate fgets() by Jean Sacren. */
+static void xfgets(char *str, int size, FILE *in)
+{
+	if (!fgets(str, size, in))
+		fprintf(stderr, "\nError in reading or end of file.\n");
 }
 
 static int conf_askvalue(struct symbol *sym, const char *def)
@@ -689,13 +695,4 @@ int main(int ac, char **av)
 		}
 	}
 	return 0;
-}
-
-/*
- * Helper function to facilitate fgets() by Jean Sacren.
- */
-void xfgets(char *str, int size, FILE *in)
-{
-	if (fgets(str, size, in) == NULL)
-		fprintf(stderr, "\nError in reading or end of file.\n");
 }
