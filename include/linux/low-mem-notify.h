fcicq/chromiumos-third_party-kernel

FIXUP: CHROMIUM: low_mem: make ram_vs_swap_weight configurable
Using do_div() on long or unsigned long is pointless and results in
a number of build warnings if compiled for a 32 bit target.

/include/linux/low-mem-notify.h: In function 'get_available_mem_adj':
./include/asm-generic/div64.h:207:28: warning:
	comparison of distinct pointer types lacks a cast
./include/linux/low-mem-notify.h:60:2: note: in expansion of macro 'do_div'
  do_div(swap_adj, low_mem_ram_vs_swap_weight);

In file included from mm/low-mem-notify.c:25:0:
./include/linux/low-mem-notify.h:60:2: warning:
	right shift count >= width of type

and so on.

BUG=chromium:727707
TEST=Build for 32-bit target and observe lack of build warning

Change-Id: I888ec8d69b1d02758010d605c091e608ed93373e
Signed-off-by: Guenter Roeck <groeck@chromium.org>
Reviewed-on: https://chromium-review.googlesource.com/671456
Reviewed-by: Luigi Semenzato <semenzato@chromium.org>

include/linux/low-mem-notify.h
@@ -55,10 +55,7 @@ static inline unsigned long get_available_mem_adj(int lru_base)
 	 * The contribution of swap is reduced by a factor of
 	 * low_mem_ram_vs_swap_weight.
 	 */
-	unsigned long swap_adj = _nr_swap_pages;
-
-	do_div(swap_adj, low_mem_ram_vs_swap_weight);
-	return available_mem + swap_adj;
+	return available_mem + _nr_swap_pages / low_mem_ram_vs_swap_weight;
 }
 
 /*

singulared/chromebook-kernel

(cherry picked from commit 655be2d59d607c8d071ee98beb5d93bb472e7a5f)
Signed-off-by: Kuo-Hsin Yang <vovoy@chromium.org>
Reviewed-on: https://chromium-review.googlesource.com/1119614
Commit-Ready: Vovo Yang <vovoy@chromium.org>
Tested-by: Vovo Yang <vovoy@chromium.org>

include/linux/low-mem-notify.h
@@ -54,10 +54,7 @@ static inline unsigned long get_available_mem_adj(int lru_base)
 	 * The contribution of swap is reduced by a factor of
 	 * low_mem_ram_vs_swap_weight.
 	 */
-	unsigned long swap_adj = nr_swap_pages;
-
-	do_div(swap_adj, low_mem_ram_vs_swap_weight);
-	return available_mem + swap_adj;
+	return available_mem + nr_swap_pages / low_mem_ram_vs_swap_weight;
 }
 
 /*